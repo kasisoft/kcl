f28f77117770701fc366095d47ef6caadcfdcad1
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2010-01-04 22:08:25 +0000

    ~ changed the constants into an enumeration type

--- com.kasisoft.lgpl.libs.common/sources/com/kasisoft/lgpl/libs/common/sys/SystemInfo.java
+++ com.kasisoft.lgpl.libs.common/sources/com/kasisoft/lgpl/libs/common/sys/SystemInfo.java
@@ -13,47 +13,50 @@ import com.kasisoft.lgpl.libs.common.constants.*;
 /**
  * Simple class that provides some system related informations.
  */
-public class SystemInfo {
-
-  private static final String VAL_OS      = SystemProperty.OsName.getValue();
+public enum SystemInfo {
   
-  private static final String OS_SUN      = "SunOS";
-  private static final String OS_SOLARIS  = "Solaris";
-  private static final String OS_LINUX    = "Linux";
-  private static final String OS_WIN95    = "Windows 95";
-  private static final String OS_WIN98    = "Windows 98";
-  private static final String OS_WINME    = "Windows Me";
-  private static final String OS_WINNT    = "Windows NT";
-  private static final String OS_WIN2000  = "Windows 2000";
-  private static final String OS_WINXP    = "Windows XP";
-  private static final String OS_WINDOWS7 = "Windows 7";
-  private static final String OS_AMIGA    = "Amiga OS";
-  private static final String OS_AROS     = "AROS";
-  private static final String OS_MACOSX   = "Mac OS X";
-  private static final String OS_MORPHOS  = "MorphOS";
+  Sun           ( "SunOS"         ),
+  Solaris       ( "Solaris"       ),
+  Linux         ( "Linux"         ),
+  Windows95     ( "Windows 95"    ),
+  Windows98     ( "Windows 98"    ),
+  WindowsME     ( "Windows Me"    ),
+  WindowsNT     ( "Windows NT"    ),
+  Windows2000   ( "Windows 2000"  ),
+  WindowsXP     ( "Windows XP"    ),
+  Windows7      ( "Windows 7"     ),
+  Amiga         ( "Amiga OS"      ),
+  Aros          ( "AROS"          ),
+  MacOSX        ( "Mac OS X"      ),
+  Morphos       ( "MorphOS"       );
+  
+  private String    key;
+  private boolean   isrunning;
   
   /**
    * Prevent instantiation.
    */
-  private SystemInfo() {
+  SystemInfo( String oskey ) {
+    key       = oskey;
+    isrunning = oskey.equals( SystemProperty.OsName.getValue() );
   }
 
   /**
-   * Returns <code>true</code> in case we're working on a Mac system.
+   * Returns <code>true</code> if this operating system is currently active.
    * 
-   * @return  <code>true</code> <=> We're working on a Mac system.
+   * @return   <code>true</code> <=> This operating system is currently active.
    */
-  public static final boolean isMac() {
-    return VAL_OS.equals( OS_MACOSX );
+  public boolean isActive() {
+    return isrunning;
   }
 
   /**
-   * Returns <code>true</code> in case we're working on a MorphOS system.
+   * Returns the key used to identify the operating system.
    * 
-   * @return  <code>true</code> <=> We're working on a MorphOS system.
+   * @return   The key used to identify the operating system. Neither <code>null</code> nor empty.
    */
-  public static final boolean isMorphos() {
-    return VAL_OS.equals( OS_MORPHOS );
+  public String getKey() {
+    return key;
   }
   
   /**
@@ -61,26 +64,8 @@ public class SystemInfo {
    *
    * @return  <code>true</code> <=> We're working on a Unix derivative.
    */
-  public static final boolean isUnix() {
-    return isSolaris() || isLinux();
-  }
-  
-  /**
-   * Returns <code>true</code> on Solaris system.
-   *
-   * @return  <code>true</code> <=> We're working on a Solaris system.
-   */
-  public static final boolean isSolaris() {
-    return VAL_OS.equals( OS_SUN ) || VAL_OS.equals( OS_SOLARIS );
-  }
-  
-  /**
-   * Returns <code>true</code> on Linux systems.
-   *
-   * @return  <code>true</code> <=> We're working on a Linux system.
-   */
-  public static final boolean isLinux() {
-    return VAL_OS.equals( OS_LINUX );
+  public boolean isUnixLike() {
+    return (this == Linux) || (this == Solaris);
   }
   
   /**
@@ -88,91 +73,11 @@ public class SystemInfo {
    *
    * @return  <code>true</code> <=> We're working on a Windows system.
    */
-  public static final boolean isWindows() {
+  public boolean isWindowsLike() {
     return
-     isWindows95   () || isWindows98   () || isWindowsMe   () || 
-     isWindows2000 () || isWindowsNT   () || isWindowsXP   ();
-  }
-  
-  /**
-   * Returns <code>true</code> on Windows 95 systems.
-   *
-   * @return  <code>true</code> <=> We're working on a Windows 95 system.
-   */
-  public static final boolean isWindows95() {
-    return VAL_OS.equals( OS_WIN95 );
-  }
-  
-  /**
-   * Returns <code>true</code> on Windows 98 systems.
-   *
-   * @return  <code>true</code> <=> We're working on a Windows 98 system.
-   */
-  public static final boolean isWindows98() {
-    return VAL_OS.equals( OS_WIN98 );
-  }
-  
-  /**
-   * Returns <code>true</code> on Windows Me systems.
-   *
-   * @return  <code>true</code> <=> We're working on a Windows Me system.
-   */
-  public static final boolean isWindowsMe() {
-    return VAL_OS.equals( OS_WINME );
-  }
-  
-  /**
-   * Returns <code>true</code> on Windows NT systems.
-   *
-   * @return  <code>true</code> <=> We're working on Windows NT systems.
-   */
-  public static final boolean isWindowsNT() {
-    return VAL_OS.equals( OS_WINNT );
-  }
-  
-  /**
-   * Returns <code>true</code> on Windows 2000 systems.
-   *
-   * @return  <code>true</code> <=> We're working on Windows 2000 systems.
-   */
-  public static final boolean isWindows2000() {
-    return VAL_OS.equals( OS_WIN2000 );
-  }
-  
-  /**
-   * Returns <code>true</code> on Windows XP systems.
-   *
-   * @return  <code>true</code> <=> We're working on Windows XP systems.
-   */
-  public static final boolean isWindowsXP() {
-    return VAL_OS.equals( OS_WINXP );
-  }
-  
-  /**
-   * Returns <code>true</code> on Windows 7 systems.
-   * 
-   * @return   <code>true</code> <=> On Windows systems. 
-   */
-  public static final boolean isWindows7() {
-    return VAL_OS.equals( OS_WINDOWS7 );
-  }
-  
-  /**
-   * Returns <code>true</code> on Amiga systems.
-   *
-   * @return  <code>true</code> <=> We're working on Amiga systems.
-   */
-  public static final boolean isAmigaOS() {
-    return VAL_OS.equals( OS_AMIGA );
-  }
-  
-  /**
-   * Returns <code>true</code> on AROS systems.
-   *
-   * @return  <code>true</code> <=> We're working on AROS systems.
-   */
-  public static final boolean isAROS() {
-    return VAL_OS.equals( OS_AROS );
+     ( this == Windows95   ) || ( this == Windows98 ) || ( this == WindowsME ) || 
+     ( this == Windows2000 ) || ( this == WindowsNT ) || ( this == WindowsXP ) ||
+     ( this == Windows7    );
   }
   
   /**
@@ -180,17 +85,24 @@ public class SystemInfo {
    * 
    * @return   <code>true</code> <=> The operating system is amiga like.
    */
-  public static final boolean isAmigaLike() {
-    return isAmigaOS() || isAROS() || isMorphos();
+  public boolean isAmigaLike() {
+    return (this == Amiga) || (this == Aros) || (this == Morphos);
   }
-  
+
   /**
-   * Returns the name of the OS.
-   *
-   * @return  The name of the OS
+   * Returns the currently actively used operating system if it could be determined. If it's not
+   * available a value of <code>null</code> is returned. Check for the property {@link SystemProperty#OsName}
+   * to get the actual key for the running operating system in that case.
+   * 
+   * @return   The constant used to identify the operating system. Maybe <code>null</code>.
    */
-  public static final String getOsName() {
-    return VAL_OS;
+  public static final SystemInfo getRunningOS() {
+    for( SystemInfo info : SystemInfo.values() ) {
+      if( info.isActive() ) {
+        return info;
+      }
+    }
+    return null;
   }
   
-} /* ENDCLASS */
+} /* ENDENUM */
