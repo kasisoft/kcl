6a759c8c16a333bd9742e67659032c19f4b1927f
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2010-01-06 22:23:44 +0000

    + test for the encoding

---
+++ com.kasisoft.lgpl.libs.common.test/sources/com/kasisoft/lgpl/libs/common/test/constants/EncodingTest.java
@@ -0,0 +1,40 @@
+/**
+ * Name........: EncodingTest
+ * Description.: Tests for the constants 'Encoding'. 
+ * Author......: Daniel Kasmeroglu
+ * E-Mail......: daniel.kasmeroglu@kasisoft.net
+ * Company.....: Kasisoft
+ * License.....: LGPL
+ */
+package com.kasisoft.lgpl.libs.common.test.constants;
+
+import org.testng.annotations.*;
+
+import org.testng.*;
+
+/**
+ * Tests for the constants 'Encoding'.
+ */
+public class EncodingTest {
+
+  @Test
+  public void encodeWithBOM() {
+    Assert.fail( "NYI" );
+  }
+  
+  @Test
+  public void encodeWithoutBOM() {
+    Assert.fail( "NYI" );
+  }
+
+  @Test
+  public void decodeWithBOM() {
+    Assert.fail( "NYI" );
+  }
+
+  @Test
+  public void decodeWithoutBOM() {
+    Assert.fail( "NYI" );
+  }
+
+} /* ENDCLASS */
