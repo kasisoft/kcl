b0fdc98500b46a5e9fa333f1e76601b516efcb33
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2010-01-07 22:43:56 +0000

    ~ added some testcases

--- com.kasisoft.lgpl.libs.common.test/sources/com/kasisoft/lgpl/libs/common/test/constants/EncodingTest.java
+++ com.kasisoft.lgpl.libs.common.test/sources/com/kasisoft/lgpl/libs/common/test/constants/EncodingTest.java
@@ -8,6 +8,8 @@
  */
 package com.kasisoft.lgpl.libs.common.test.constants;
 
+import com.kasisoft.lgpl.libs.common.constants.*;
+
 import org.testng.annotations.*;
 
 import org.testng.*;
@@ -17,24 +19,20 @@ import org.testng.*;
  */
 public class EncodingTest {
 
-  @Test
-  public void encodeWithBOM() {
-    Assert.fail( "NYI" );
+  @DataProvider(name="createData")
+  public Object[][] createData() {
+    return new Object[][] {
+      { "Flöz", Encoding.UTF8, new byte[] { (byte) 0x46, (byte) 0x6C, (byte) 0xC3, (byte) 0xB6, (byte) 0x7A } },
+      { "Flöz", Encoding.UTF16, new byte[] { (byte) 0xFE, (byte) 0xFF, (byte) 0x00, (byte) 0x46, (byte) 0x00, (byte) 0x6C, (byte) 0x00, (byte) 0xF6, (byte) 0x00, (byte) 0x7A } },
+      { "Flöz", Encoding.UTF16BE, new byte[] { (byte) 0x00, (byte) 0x46, (byte) 0x00, (byte) 0x6C, (byte) 0x00, (byte) 0xF6, (byte) 0x00, (byte) 0x7A } },
+      { "Flöz", Encoding.UTF16LE, new byte[] { (byte) 0x46, (byte) 0x00, (byte) 0x6C, (byte) 0x00, (byte) 0xF6, (byte) 0x00, (byte) 0x7A, (byte) 0x00  } },
+    };
+  };
+  
+  @Test(dataProvider="createData")
+  public void performEncoding( String literal, Encoding encoding, byte[] bytes ) {
+    byte[] encoded = encoding.encode( literal );
+    Assert.assertEquals( encoded, bytes );
   }
   
-  @Test
-  public void encodeWithoutBOM() {
-    Assert.fail( "NYI" );
-  }
-
-  @Test
-  public void decodeWithBOM() {
-    Assert.fail( "NYI" );
-  }
-
-  @Test
-  public void decodeWithoutBOM() {
-    Assert.fail( "NYI" );
-  }
-
 } /* ENDCLASS */
