2c8b5d075bce2c2dd61ca6b5ae4c9ed5532bb135
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2010-01-11 21:32:46 +0000

    + collection of constants used to identify failures

---
+++ com.kasisoft.lgpl.libs.common/sources/com/kasisoft/lgpl/libs/common/constants/FailureCodes.java
@@ -0,0 +1,68 @@
+/**
+ * Name........: FailureCodes
+ * Description.: Collection of failure codes.
+ * Author......: Daniel Kasmeroglu
+ * E-Mail......: daniel.kasmeroglu@kasisoft.net
+ * Company.....: Kasisoft
+ * License.....: LGPL
+ */
+package com.kasisoft.lgpl.libs.common.constants;
+
+/**
+ * Collection of failure codes.
+ */
+public enum FailureCodes {
+
+  /** everythings okay. */
+  Success                                             (  0 ),
+  
+  /** an unknown failure has been launched. */
+  Unknown                                             ( -1 ),
+  
+  /** closing a resource failed. */
+  Close                                               ( -2 ),
+  
+  /** reading or writing failed. */
+  IO                                                  ( -3 ),
+  
+  /** skipping within a resource failed. */
+  Skip                                                ( -4 ),
+  
+  /** the encoding that has been used is not supported. */
+  InvalidEncoding                                     ( -5 ),
+
+  /** a timeout has been recognized. */
+  Timeout                                             ( -6 ),
+
+  /** a listener implementation caused a failure. */
+  ListenerFailure                                     ( -7 ),
+  
+  /** a reflections based functionality failed. */
+  Reflections                                         ( -8 ),
+  
+  /** indicates a xml related failure. */
+  XmlFailure                                          ( -9 );
+
+  private int   code;
+  
+  FailureCodes( int value ) {
+    code = value;
+  }
+  
+  /**
+   * Returns the numeric failure code associated with this constant.
+   * 
+   * @return   The numeric failure code associated with this constant.
+   */
+  public int getCode() {
+    return code;
+  }
+  
+  /**
+   * {@inheritDoc}
+   */
+  public String toString() {
+    return String.format( "%s[%d]", name(), Integer.valueOf( code ) );
+  }
+  
+} /* ENDENUM */
