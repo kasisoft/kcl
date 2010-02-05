f5bef7c558ed6fa1c97fbd7ecc4bf46e6e5aa1d7
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2010-02-05 23:01:26 +0000

    + initial testcase for zip and unzip

---
+++ com.kasisoft.lgpl.libs.common.test/sources/com/kasisoft/lgpl/libs/common/test/thread/ZipAndUnzipRunnableTest.java
@@ -0,0 +1,79 @@
+/**
+ * Name........: ZipAndUnzipRunnableTest
+ * Description.: Test for the classes 'ZipRunnable' and 'UnzipRunnable'.
+ * Author......: Daniel Kasmeroglu
+ * E-Mail......: daniel.kasmeroglu@kasisoft.net
+ * Company.....: Kasisoft
+ * License.....: LGPL
+ */
+package com.kasisoft.lgpl.libs.common.test.thread;
+
+import com.kasisoft.lgpl.libs.common.thread.*;
+
+import com.kasisoft.lgpl.libs.common.io.*;
+
+import org.testng.annotations.*;
+
+import org.testng.*;
+
+import java.util.*;
+
+import java.io.*;
+
+/**
+ * Test for the classes 'ZipRunnable' and 'UnzipRunnable'.
+ */
+public class ZipAndUnzipRunnableTest extends AbstractFileSystemTest {
+  
+  private File   directory;
+  private File   unpackeddir;
+  private File   destfile;
+  
+  @SuppressWarnings("unused")
+  @BeforeClass
+  private void setup() {
+    destfile      = IoFunctions.newTempFile( "file-", ".zip" );
+    directory     = IoFunctions.newTempFile( "temp-" );
+    directory.mkdirs();
+    unpackeddir   = IoFunctions.newTempFile( "temp-" );
+    unpackeddir.mkdirs();
+    createFileSystemStructure( directory );
+  }
+  
+  @Test
+  public void zip() throws InterruptedException {
+    ZipRunnable runnable = new ZipRunnable( destfile, directory );
+    Thread      thread   = new Thread( runnable );
+    thread.start();
+    thread.join();
+    Assert.assertTrue( runnable.hasCompleted() );
+  }
+  
+  @Test(dependsOnMethods="zip")
+  public void unzip() throws InterruptedException {
+    UnzipRunnable runnable = new UnzipRunnable( destfile, unpackeddir );
+    Thread        thread   = new Thread( runnable );
+    thread.start();
+    thread.join();
+    Assert.assertTrue( runnable.hasCompleted() );
+    Assert.assertEquals( unpackeddir, directory );
+  }
+  
+  @Test
+  public void deleteFiles() throws InterruptedException {
+    File                tempdir   = IoFunctions.newTempFile( "temp-", null );
+    tempdir.mkdirs();
+    List<File>          created   = createFileSystemStructure( tempdir );
+    Collections.sort( created );
+    FileListRunnable    runnable  = new FileListRunnable( tempdir.listFiles() );
+    Thread              thread    = new Thread( runnable );
+    thread.start();
+    thread.join();
+    Assert.assertTrue( runnable.hasCompleted() );
+    List<File> files = runnable.getAllFiles();
+    Assert.assertEquals( files.size(), created.size() );
+    Collections.sort( files );
+    Assert.assertEquals( files, created );
+  }
+  
+} /* ENDCLASS */
