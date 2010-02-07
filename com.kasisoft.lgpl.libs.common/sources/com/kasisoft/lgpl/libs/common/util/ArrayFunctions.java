f1d41989f513f84f924a137c75ca9f751f0e7a17
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2010-02-07 23:20:53 +0000

    + collection of array related utility functions

---
+++ com.kasisoft.lgpl.libs.common/sources/com/kasisoft/lgpl/libs/common/util/ArrayFunctions.java
@@ -0,0 +1,360 @@
+/**
+ * Name........: ArrayFunctions
+ * Description.: Collection of functions useful in conjunction with arrays. 
+ * Author......: Daniel Kasmeroglu
+ * E-Mail......: daniel.kasmeroglu@kasisoft.net
+ * Company.....: Kasisoft
+ * License.....: LGPL
+ */
+package com.kasisoft.lgpl.libs.common.util;
+
+import com.kasisoft.lgpl.tools.diagnostic.*;
+
+import java.util.*;
+
+/**
+ * Collection of functions useful in conjunction with arrays.
+ */
+@KDiagnostic
+public class ArrayFunctions {
+
+  /**
+   * Prevent instantiation.
+   */
+  private ArrayFunctions() {
+  }
+  
+  /**
+   * Returns the maximum of the supplied values.
+   * 
+   * @param args   The values used to determine the maximum. Must have at least the length 1.
+   * 
+   * @return   The maximum of all values.
+   */
+  public static final int maxInt( 
+    @KNotEmpty(name="args")   int ... args 
+  ) {
+    if( args.length == 1 ) {
+      return args[0];
+    }
+    int result = Math.max( args[0], args[1] );
+    for( int i = 2; i < args.length; i++ ) {
+      result = Math.max( args[i], result );
+    }
+    return result;
+  }
+
+  /**
+   * Returns the maximum of the supplied values.
+   * 
+   * @param args   The values used to determine the maximum. Must have at least the length 1.
+   * 
+   * @return   The maximum of all values.
+   */
+  public static final long maxLong( 
+    @KNotEmpty(name="args")   long ... args 
+  ) {
+    if( args.length == 1 ) {
+      return args[0];
+    }
+    long result = Math.max( args[0], args[1] );
+    for( int i = 2; i < args.length; i++ ) {
+      result = Math.max( args[i], result );
+    }
+    return result;
+  }
+
+  /**
+   * Returns the maximum of the supplied values.
+   * 
+   * @param args   The values used to determine the maximum. Must have at least the length 1.
+   * 
+   * @return   The maximum of all values.
+   */
+  public static final float maxFloat( 
+    @KNotEmpty(name="args")   float ... args
+  ) {
+    if( args.length == 1 ) {
+      return args[0];
+    }
+    float result = Math.max( args[0], args[1] );
+    for( int i = 2; i < args.length; i++ ) {
+      result = Math.max( args[i], result );
+    }
+    return result;
+  }
+
+  /**
+   * Returns the maximum of the supplied values.
+   * 
+   * @param args   The values used to determine the maximum. Must have at least the length 1.
+   * 
+   * @return   The maximum of all values.
+   */
+  public static final double maxDouble( 
+    @KNotEmpty(name="args")   double ... args 
+  ) {
+    if( args.length == 1 ) {
+      return args[0];
+    }
+    double result = Math.max( args[0], args[1] );
+    for( int i = 2; i < args.length; i++ ) {
+      result = Math.max( args[i], result );
+    }
+    return result;
+  }
+
+  /**
+   * Returns the minimum of the supplied values.
+   * 
+   * @param args   The values used to determine the minimum. Must have at least the length 1.
+   * 
+   * @return   The minimum of all values.
+   */
+  public static final int minInt( 
+    @KNotEmpty(name="args")   int ... args 
+  ) {
+    if( args.length == 1 ) {
+      return args[0];
+    }
+    int result = Math.min( args[0], args[1] );
+    for( int i = 2; i < args.length; i++ ) {
+      result = Math.min( args[i], result );
+    }
+    return result;
+  }
+
+  /**
+   * Returns the minimum of the supplied values.
+   * 
+   * @param args   The values used to determine the minimum. Must have at least the length 1.
+   * 
+   * @return   The minimum of all values.
+   */
+  public static final long minLong( 
+    @KNotEmpty(name="args")   long ... args 
+  ) {
+    if( args.length == 1 ) {
+      return args[0];
+    }
+    long result = Math.min( args[0], args[1] );
+    for( int i = 2; i < args.length; i++ ) {
+      result = Math.min( args[i], result );
+    }
+    return result;
+  }
+
+  /**
+   * Returns the minimum of the supplied values.
+   * 
+   * @param args   The values used to determine the minimum. Must have at least the length 1.
+   * 
+   * @return   The minimum of all values.
+   */
+  public static final float minFloat(
+    @KNotEmpty(name="args")   float ... args 
+  ) {
+    if( args.length == 1 ) {
+      return args[0];
+    }
+    float result = Math.min( args[0], args[1] );
+    for( int i = 2; i < args.length; i++ ) {
+      result = Math.min( args[i], result );
+    }
+    return result;
+  }
+
+  /**
+   * Returns the minimum of the supplied values.
+   * 
+   * @param args   The values used to determine the minimum. Must have at least the length 1.
+   * 
+   * @return   The minimum of all values.
+   */
+  public static final double minDouble( 
+    @KNotEmpty(name="args")   double ... args 
+  ) {
+    if( args.length == 1 ) {
+      return args[0];
+    }
+    double result = Math.min( args[0], args[1] );
+    for( int i = 2; i < args.length; i++ ) {
+      result = Math.min( args[i], result );
+    }
+    return result;
+  }
+  
+  /**
+   * Runs the AND operation for the supplied values.
+   * 
+   * @param atoms   Atomic expressions. Not <code>null</code>.
+   * 
+   * @return   The boolean result. Not <code>null</code>.
+   */
+  public static final Boolean objectAnd( 
+    @KNotEmpty(name="atoms")   Boolean ... atoms 
+  ) {
+    boolean result = atoms[0].booleanValue();
+    for( int i = 0; (i < atoms.length) && result; i++ ) {
+      result = result && atoms[i].booleanValue();
+    }
+    return Boolean.valueOf( result );
+  }
+
+  /**
+   * Runs the OR operation for the supplied values.
+   * 
+   * @param atoms   Atomic expressions. Not <code>null</code>.
+   * 
+   * @return   The boolean result. Not <code>null</code>.
+   */
+  public static final Boolean objectOr( 
+    @KNotEmpty(name="atoms")   Boolean ... atoms 
+  ) {
+    boolean result = atoms[0].booleanValue();
+    for( int i = 0; (i < atoms.length) && (! result); i++ ) {
+      result = result || atoms[i].booleanValue();
+    }
+    return Boolean.valueOf( result );
+  }
+
+  /**
+   * Determines the and'ed result of the supplied arguments.
+   * 
+   * @param args   A list of booleans to be combined. Must have at least the length 1.
+   * 
+   * @return   <code>true</code> <=> Each argument was <code>true</code>.
+   */
+  public static final boolean and( 
+    @KNotEmpty(name="args")  boolean ... args
+  ) {
+    boolean result = args[0];
+    for( int i = 1; (i < args.length) && result; i++ ) {
+      result = result && args[i];
+    }
+    return result;
+  }
+
+  /**
+   * Determines the or'ed result of the supplied arguments.
+   * 
+   * @param args   A list of booleans to be combined. Must have at least the length 1.
+   * 
+   * @return   <code>true</code> <=> At least one argument was <code>true</code>.
+   */
+  public static final boolean or( 
+    @KNotEmpty(name="args")   boolean ... args
+  ) {
+    boolean result = args[0];
+    for( int i = 1; (i < args.length) && (! result); i++ ) {
+      result = result || args[i];
+    }
+    return result;
+  }
+
+  /**
+   * Extendss a list with an array and returns it. The order is preserved. The list will be extended
+   * so any previously existing content remains there.
+   * 
+   * @param receiver   The list which will be extended. Not <code>null</code>.
+   * @param input      The data items which have to be added. Maybe <code>null</code>.
+   * 
+   * @return   The list that has been supplied. Not <code>null</code>.
+   */
+  public static final <T> List<T> addAll( 
+    @KNotNull(name="receiver")   List<T>     receiver, 
+                                 T       ... input 
+  ) {
+    if( input != null ) {
+      for( int i = 0; i < input.length; i++ ) {
+        receiver.add( input[i] );
+      }
+    }
+    return receiver;
+  }
+  
+  /**
+   * Returns an Enumeration which is usable to iterate through the supplied array.
+   * 
+   * @param input   The array which should be traversed.
+   * 
+   * @return   The enumeration which is used to traverse the array.
+   */
+  public static final <T> Enumeration<T> enumeration( 
+    @KNotNull(name="input")   T ... input 
+  ) {
+    return new ArrayTraversal<T>( input );
+  }
+
+  /**
+   * Returns an Iterator which is usable to iterate through the supplied array.
+   * 
+   * @param input   The array which should be traversed.
+   * 
+   * @return   The Iterator which is used to traverse the array.
+   */
+  public static final <T> Iterator<T> iterator( T ... input ) {
+    return new ArrayTraversal<T>( input );
+  }
+
+  /**
+   * Implementation of the interfaces Enumeration and Iterator allowing to traverse arrays.
+   */
+  private static class ArrayTraversal<T> implements Enumeration<T>, Iterator<T> {
+    
+    private T[]   data;
+    private int   index;
+    
+    /**
+     * Initialises this traversal class.
+     * 
+     * @param input   The array which can be traversed.
+     */
+    public ArrayTraversal( T[] input ) {
+      data  = input;
+      index = 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean hasMoreElements() {
+      return index < data.length;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean hasNext() {
+      return hasMoreElements();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public T nextElement() {
+      if( index >= data.length ) {
+        throw new NoSuchElementException();
+      }
+      T result = data[ index ];
+      index++;
+      return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public T next() {
+      return nextElement();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void remove() {
+      throw new UnsupportedOperationException();
+    }
+    
+  } /* ENDCLASS */
+  
+} /* ENDCLASS */
