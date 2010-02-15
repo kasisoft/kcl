554ff38caa9c5c042fff2fdea95eeacc837c1267
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2010-02-15 21:12:26 +0000

    ~ modifications according to the changed content of the 'testdata' directory

--- com.kasisoft.lgpl.libs.common.test/sources/com/kasisoft/lgpl/libs/common/test/io/IoFunctionsTest.java
+++ com.kasisoft.lgpl.libs.common.test/sources/com/kasisoft/lgpl/libs/common/test/io/IoFunctionsTest.java
@@ -359,10 +359,10 @@ public class IoFunctionsTest {
     };
     
     List<File> list1  = IoFunctions.listRecursive( testdata, filter );
-    Assert.assertEquals( list1.size(), 15 );
+    Assert.assertEquals( list1.size(), 17 );
 
     List<File> list2  = IoFunctions.listRecursive( testdata, filter, true, false );
-    Assert.assertEquals( list2.size(), 9 );
+    Assert.assertEquals( list2.size(), 11 );
 
     List<File> list3  = IoFunctions.listRecursive( testdata, filter, false, true );
     Assert.assertEquals( list3.size(), 6 );
