d514004a1d88c6b5801ff3ffadfc274a0a49fe51
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2010-02-15 21:09:27 +0000

    ~ resources will be loaded when they're registered

--- com.kasisoft.lgpl.libs.common/sources/com/kasisoft/lgpl/libs/common/xml/XmlCatalog.java
+++ com.kasisoft.lgpl.libs.common/sources/com/kasisoft/lgpl/libs/common/xml/XmlCatalog.java
@@ -10,22 +10,18 @@ package com.kasisoft.lgpl.libs.common.xml;
 
 import com.kasisoft.lgpl.libs.common.io.*;
 
+import com.kasisoft.lgpl.libs.common.base.*;
 import com.kasisoft.lgpl.tools.diagnostic.*;
 
 import java.util.*;
 
 import java.net.*;
 
-import java.io.*;
-
 /**
  * Basic data structure used to store entity ids together with the urls.
  */
 public class XmlCatalog {
 
-  private static final byte[] NO_DATA = new byte[0];
-  
-  private Map<String,URL>       catalogrefs;
   private Map<String,byte[]>    catalogdata;
   
   /**
@@ -33,7 +29,6 @@ public class XmlCatalog {
    */
   public XmlCatalog() {
     catalogdata = new Hashtable<String,byte[]>();
-    catalogrefs = new Hashtable<String,URL>();
   }
   
   /**
@@ -46,7 +41,13 @@ public class XmlCatalog {
     @KNotEmpty(name="id")   String   id, 
     @KNotNull(name="url")   URL      url 
   ) {
-    catalogrefs.put( id, url );
+    try {
+      byte[] data = IoFunctions.loadBytes( url, null );
+      catalogdata.put( id, data );
+    } catch( FailureException ex ) {
+      // we're ignoring this which means that we weren't capable to access the resource
+      // but the resolving process still might succeed
+    }
   }
 
   /**
@@ -55,7 +56,21 @@ public class XmlCatalog {
    * @param systemid   The resource which has to be registered. Not <code>null</code>.
    */
   public synchronized void registerSystemID( @KNotNull(name="systemid") URL systemid ) {
-    catalogrefs.put( systemid.toExternalForm(), systemid );
+    try {
+      byte[]  data = IoFunctions.loadBytes( systemid, null );
+      catalogdata.put( systemid.toExternalForm(), data );
+      // we're also registering the resource name which might also be used to match
+      String  path = systemid.getPath();
+      int     lidx = path.lastIndexOf( '/' );
+      if( lidx != -1 ) {
+        catalogdata.put( path.substring( lidx + 1 ), data );
+      } else {
+        catalogdata.put( path, data );
+      }
+    } catch( FailureException ex ) {
+      // we're ignoring this which means that we weren't capable to access the resource
+      // but the resolving process still might succeed
+    }
   }
   
   /**
@@ -66,7 +81,7 @@ public class XmlCatalog {
    * @return   The content if it could be loaded. Maybe <code>null</code>.
    */
   public synchronized byte[] loadResource( @KNotEmpty(name="publicid") String publicid ) {
-    return getResource( publicid );
+    return catalogdata.get( publicid );
   }
 
   /**
@@ -77,47 +92,17 @@ public class XmlCatalog {
    * @return   The content if it could be loaded. Maybe <code>null</code>.
    */
   public synchronized byte[] loadResource( @KNotNull(name="resource") URL resource ) {
-    return getResource( resource.toExternalForm() );
-  }
-  
-  /**
-   * Loads the content associated through the supplied resource.
-   * 
-   * @param key   The key used to access the resource content. Neither <code>null</code> nor empty.
-   * 
-   * @return   The data of the resource that has been loaded. Maybe <code>null</code>.
-   */
-  private byte[] getResource( String key ) {
-    
-    // check whether we've already got the data
-    byte[] result = catalogdata.get( key );
-    if( result != null ) {
-      if( result == NO_DATA ) {
-        // this is a placeholder to indicate a failure of a previous load, so
-        // we don't need to fail over and over again
-        return null;
+    if( catalogdata.containsKey( resource ) ) {
+      return catalogdata.get( resource );
+    } else {
+      String path = resource.getPath();
+      int    lidx = path.lastIndexOf( '/' );
+      if( lidx != -1 ) {
+        return catalogdata.get( path.substring( lidx + 1 ) );
       } else {
-        return result;
+        return catalogdata.get( path );
       }
     }
-    
-    URL         resource = catalogrefs.get( key );
-    if( resource == null ) {
-      // no registration has taken place before
-      return null;
-    }
-    InputStream instream = null;
-    try {
-      instream  = resource.openStream();
-      result    = IoFunctions.loadBytes( instream, null );
-      catalogdata.put( key, result );
-    } catch( IOException ex ) {
-      catalogdata.put( key, NO_DATA );
-    } finally {
-      IoFunctions.close( instream );
-    }    
-    return result;
-    
   }
   
 } /* ENDCLASS */
