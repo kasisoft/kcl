203337db9e5bd2866ad09121472064f39b4e91c4
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2010-02-16 20:44:59 +0000

    + added the enumeration 'MimeType'

---
+++ com.kasisoft.lgpl.libs.common/sources/com/kasisoft/lgpl/libs/common/constants/MimeType.java
@@ -0,0 +1,137 @@
+/**
+ * Name........: MimeType
+ * Description.: 
+ * Author......: Daniel Kasmeroglu
+ * E-Mail......: daniel.kasmeroglu@kasisoft.net
+ * Company.....: Kasisoft
+ * License.....: LGPL
+ */
+package com.kasisoft.lgpl.libs.common.constants;
+
+import com.kasisoft.lgpl.tools.diagnostic.*;
+
+import java.util.*;
+
+/**
+ * @source [15-Feb-2010:KASI]   http://de.selfhtml.org/diverses/mimetypen.htm
+ * 
+ * Alternate and more official: @source [15-Feb-2010:KASI]   http://www.iana.org/assignments/media-types
+ * 
+ * @note [15-Feb-2010:KASI]   Not all types have been used here.
+ */
+public enum MimeType {
+
+  GZip                        ( "application/gzip"              , "gz"                                ),  // GNU Zip
+  MicrosoftExcel              ( "application/msexcel"           , "xls", "xla"                        ),  // Microsoft Excel
+  MicrosoftHelp               ( "application/mshelp"            , "hlp", "chm"                        ),  // Microsoft help files
+  MicrosoftPowerpoint         ( "application/mspowerpoint"      , "ppt", "ppz", "pps", "pot"          ),  // Microsoft Powerpoint
+  MicrosoftWord               ( "application/msword"            , "doc", "dot"                        ),  // Microsoft Word
+  OctetStream                 ( "application/octet-stream"      , "bin", "exe", "com", "dll", "class" ),  // Executable
+  AdobePdf                    ( "application/pdf"               , "pdf"                               ),  // Adobe PDF
+  Postscript                  ( "application/postscript"        , "ai", "eps", "ps"                   ),  // Adobe Postscript
+  XhtmlXml                    ( "application/xhtml+xml"         , "htm", "html", "shtml", "xhtml"     ),  // XHTML
+  ZLib                        ( "application/x-compress"        , "z"                                 ),  // ZLib compressed files 
+  Dvi                         ( "application/x-dvi"             , "dvi"                               ),  // DVI
+  GnuTar                      ( "application/x-gtar"            , "gtar"                              ),  // GNU Tar
+  Php                         ( "application/x-httpd-php"       , "php", "phtml"                      ),  // PHP
+  LaTeX                       ( "application/x-latex"           , "latex", "tex"                      ),  // LaTeX
+  Tar                         ( "application/x-tar"             , "tar"                               ),  // Tape-Archive
+  TeX                         ( "application/x-tex"             , "tex"                               ),  // TeX
+  Zip                         ( "application/zip"               , "zip"                               ),  // ZIP archives
+  Wave                        ( "audio/x-wav"                   , "wav"                               ),  // WAV-Files
+  Gif                         ( "image/gif"                     , "gif"                               ),  // GIF-Files
+  Jpeg                        ( "image/jpeg"                    , "jpeg", "jpg", "jpe"                ),  // JPEG-Files
+  Png                         ( "image/png"                     , "png"                               ),  // PNG-Files
+  Tiff                        ( "image/tiff"                    , "tiff", "tif"                       ),  // TIFF-Files
+  Icon                        ( "image/x-icon"                  , "ico"                               ),  // Icon-Files
+  CommaSeparatedValues        ( "text/comma-separated-values"   , "csv"                               ),  // comma separated values
+  CascadingStylesheet         ( "text/css"                      , "css"                               ),  // cascading stylesheets
+  Html                        ( "text/html"                     , "htm", "html", "shtml"              ),  // HTML files
+  Javascript                  ( "text/javascript"               , "js"                                ),  // JavaScript-Files
+  Plaintext                   ( "text/plain"                    , "txt", "text"                       ),  // plain text files
+  MicrosoftRTF                ( "text/rtf"                      , "rtf"                               ),  // Microsoft RTF
+  TabSeparatedValues          ( "text/tab-separated-values"     , "tsv"                               ),  // tabulator-separated files
+  Xml                         ( "text/xml"                      , "xml"                               ),  // xml files
+  Sgml                        ( "text/x-sgml"                   , "sgm", "sgml"                       );  // SGML-files
+
+  private String     mimetype;
+  private String[]   suffices;
+  
+  MimeType( String type, String ... suffixlist ) {
+    mimetype  = type;
+    suffices  = suffixlist;
+    if( suffices == null ) {
+      suffices  = new String[0];
+    }
+  }
+  
+  /**
+   * Returns the mime type as it appears in internet related messages/documents.
+   * 
+   * @return   The mime type as it appears in internet related messages/documents. 
+   *           Neither <code>null</code> nor empty.
+   */
+  public String getMimeType() {
+    return mimetype;
+  }
+  
+  /**
+   * Returns a list of suffices supported by this mime type. The suffices don't contain the dot
+   * and are all lowercase.
+   * 
+   * @return   A list of suffices supported by this mime type. Not <code>null</code>.
+   */
+  public List<String> getSuffices() {
+    return Arrays.asList( suffices );
+  }
+  
+  /**
+   * Returns <code>true</Code> if the supplied suffix is support by this mime type.
+   * 
+   * @param suffix   The suffix that has to be tested. Neither <code>null</code> nor empty.
+   * 
+   * @return   <code>true</code> <=> The supplied suffix is supported by this mime type.
+   */
+  public boolean supportsSuffix( @KNotEmpty(name="suffix") String suffix ) {
+    for( String current : suffices ) {
+      if( current.equalsIgnoreCase( suffix ) ) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  /**
+   * Returns the MimeType constant that is associated with the supplied type.
+   * 
+   * @param type   The current mime type which has to be identified. Neither <code>null</code> nor empty.
+   * 
+   * @return   The MimeType if it could be found or <code>null</code>.
+   */
+  public static final MimeType valueByMimeType( @KNotEmpty(name="type") String type ) {
+    for( MimeType mime : MimeType.values() ) {
+      if( type.equalsIgnoreCase( mime.mimetype ) ) {
+        return mime;
+      }
+    }
+    return null;
+  }
+  
+  /**
+   * Returns a set of known mime types supporting the supplied suffix.
+   * 
+   * @param suffix   The suffix used to identify a filetype. Neither <code>null</code> nor empty.
+   * 
+   * @return   A set of supporting mime types. Not <code>null</code>.
+   */
+  public static final Set<MimeType> valuesBySuffix( @KNotEmpty(name="suffix") String suffix ) {
+    Set<MimeType> result = new HashSet<MimeType>();
+    for( MimeType mime : MimeType.values() ) {
+      if( mime.supportsSuffix( suffix ) ) {
+        result.add( mime );
+      }
+    }
+    return result;
+  }
+  
+} /* ENDENUM */
