1589e5a244aef35fbaa49a61815160bdbb74a161
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2010-12-09 23:04:12 +0000

    ~ modified this handler so the source of the error is passed, too

--- com.kasisoft.lgpl.libs.common/sources/com/kasisoft/lgpl/libs/common/util/SimpleErrorHandler.java
+++ com.kasisoft.lgpl.libs.common/sources/com/kasisoft/lgpl/libs/common/util/SimpleErrorHandler.java
@@ -16,10 +16,11 @@ public interface SimpleErrorHandler {
   /**
    * Will be called whenever an error happened.
    * 
+   * @param source    The object that invokes this message. Not <code>null</code>.
    * @param message   The message that provides further information. 
    *                  Neither <code>null</code> nor empty.
    * @param cause     A potential cause of the error. Maybe <code>null</code>.
    */
-  void failure( String message, Exception cause );
+  void failure( Object source, String message, Exception cause );
   
 } /* ENDINTERFACE */
