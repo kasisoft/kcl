848e3ad34e7ed856d897e524b8ad5c3fda5a831d
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2010-12-10 20:08:34 +0000

    + added a type for integer types

---
+++ com.kasisoft.lgpl.libs.common/sources/com/kasisoft/lgpl/libs/common/xml/adapters/IntegerAdapter.java
@@ -0,0 +1,40 @@
+/**
+ * Name........: IntegerAdapter
+ * Description.: Simple adapter for integer types.
+ * Author......: Daniel Kasmeroglu
+ * E-Mail......: daniel.kasmeroglu@kasisoft.net
+ * Company.....: Kasisoft
+ * License.....: LGPL
+ */
+package com.kasisoft.lgpl.libs.common.xml.adapters;
+
+
+/**
+ * Simple adapter for integer types.
+ */
+public class IntegerAdapter extends NullSafeAdapter<String,Integer> {
+
+  private static final String MAX   = "MAX";
+  private static final String MIN   = "MIN";
+  
+  /**
+   * {@inheritDoc}
+   */
+  public String marshalImpl( Integer v ) {
+    return v.toString();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public Integer unmarshalImpl( String v ) {
+    if( MAX.equalsIgnoreCase( v ) ) {
+      return Integer.valueOf( Integer.MAX_VALUE );
+    } else if( MIN.equalsIgnoreCase( v ) ) {
+      return Integer.valueOf( Integer.MIN_VALUE );
+    } else {
+      return Integer.valueOf( v );
+    }
+  }
+
+} /* ENDCLASS */
