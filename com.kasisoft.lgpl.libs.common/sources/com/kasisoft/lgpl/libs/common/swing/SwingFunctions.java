fe688cd132647812da0ba9821e012f707d2b98ee
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2010-12-11 22:44:49 +0000

    + initial port of custom swing related utility functions

---
+++ com.kasisoft.lgpl.libs.common/sources/com/kasisoft/lgpl/libs/common/swing/SwingFunctions.java
@@ -0,0 +1,58 @@
+/**
+ * Name........: SwingFunctions
+ * Description.: Collection of Swing related utility functions.
+ * Author......: Daniel Kasmeroglu
+ * E-Mail......: daniel.kasmeroglu@kasisoft.net
+ * Company.....: Kasisoft
+ * License.....: LGPL
+ */
+package com.kasisoft.lgpl.libs.common.swing;
+
+import com.kasisoft.lgpl.tools.diagnostic.*;
+
+import java.awt.*;
+
+/**
+ * Collection of Swing related utility functions.
+ */
+@KDiagnostic(loggername="com.kasisoft.lgpl.libs.common")
+public class SwingFunctions {
+
+  
+  /**
+   * Modifies the boundaries of a component so it's being centered related to another component.
+   * 
+   * @param relocatable   The component which boundaries have to be adjusted. Not <code>null</code>.
+   * @param related       The related container. If <code>null</code> the screen dimensions are used.
+   *                      You can alternatively use {@link #center(Component)}.
+   */
+  public static final void center( @KNotNull(name="relocatable") Component relocatable, Component related ) {
+    if( related == null ) {
+        center( relocatable );
+    } else {
+      Dimension relosize    = relocatable.getSize();
+      Dimension relatedsize = related.getSize();
+      int       width       = relatedsize.width  - relosize.width;
+      int       height      = relatedsize.height - relosize.height;
+      Point     location    = related.getLocation();
+      relocatable.setBounds( location.x + width / 2, location.y + height / 2, relosize.width, relosize.height );
+    }
+  }
+  
+
+  /**
+   * Modifies the boundaries of the supplied component so it will be centered on the screen. If the
+   * size of the component exceeds the size of the screen it's location will be modified accordingly
+   * to make sure it's in a visible area.
+   * 
+   * @param relocatable   The component which boundaries have to be adjusted. Not <code>null</code>.
+   */
+  public static final void center( @KNotNull(name="relocatable") Component relocatable ) {
+    Dimension relosize    = relocatable.getSize();
+    Dimension screensize  = Toolkit.getDefaultToolkit().getScreenSize();
+    int       width       = screensize.width  - relosize.width;
+    int       height      = screensize.height - relosize.height;
+    relocatable.setBounds( Math.max( width / 2, 0 ), Math.max( height / 2, 0 ), relosize.width, relosize.height );
+  }
+
+} /* ENDCLASS */
