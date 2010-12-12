51c3544c4c44d1f2a958da06e56e62f4c5c7190f
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2010-12-12 20:54:33 +0000

    + added some methods to set/get typed properties

--- com.kasisoft.lgpl.libs.common/sources/com/kasisoft/lgpl/libs/common/workspace/Workspace.java
+++ com.kasisoft.lgpl.libs.common/sources/com/kasisoft/lgpl/libs/common/workspace/Workspace.java
@@ -12,10 +12,18 @@ import com.kasisoft.lgpl.libs.common.constants.*;
 
 import com.kasisoft.lgpl.libs.common.util.*;
 
+import com.kasisoft.lgpl.libs.common.xml.adapters.*;
+
 import com.kasisoft.lgpl.libs.common.base.*;
 import com.kasisoft.lgpl.tools.diagnostic.*;
 
+import javax.xml.bind.annotation.adapters.*;
+
+import java.util.*;
+
 import java.io.*;
+
+import java.awt.*;
 
 /**
  * The Workspace allows to store various configuration information during the runtime.
@@ -25,9 +33,10 @@ public final class Workspace {
 
   private static Workspace       instance = null;
   
-  private File            settingsfile;
-  private ExtProperties   properties;
-  private boolean         isnew;
+  private File                       settingsfile;
+  private ExtProperties              properties;
+  private boolean                    isnew;
+  private Map<Class<?>,XmlAdapter>   adapters;
   
   /**
    * Initialises this workspace used for temporary savings.
@@ -36,6 +45,8 @@ public final class Workspace {
     settingsfile  = null;
     isnew         = false;
     properties    = new ExtProperties();
+    adapters      = new Hashtable<Class<?>,XmlAdapter>();
+    adapters.put( Rectangle.class, new RectangleAdapter( ":" ) );
   }
   
   /**
@@ -84,6 +95,115 @@ public final class Workspace {
    */
   private void loadSettings() throws FailureException {
     properties.load( settingsfile, Encoding.UTF8 );
+  }
+  
+  /**
+   * Changes the value for a textual property.
+   * 
+   * @param property   The property key which has to be used. Neither <code>null</code> nor empty.
+   * @param value      The value which has to be saved. Maybe <code>null</code>.
+   */
+  public void setString( @KNotEmpty(name="property") String property, String value ) {
+    properties.setProperty( property, value );
+  }
+  
+  /**
+   * Returns a value associated with a specific property key.
+   * 
+   * @param property   The property key which has to be used. Neither <code>null</code> nor empty.
+   * @param defvalue   The default value to be used if there's no value yet. Maybe <code>null</code>.
+   * 
+   * @return   The value associated with the property. Maybe <code>null</code>.
+   */
+  public String getString( @KNotEmpty(name="property") String property, String defvalue ) {
+    return properties.getProperty( property, defvalue );
+  }
+
+  /**
+   * Returns a value associated with a specific property key.
+   * 
+   * @param property   The property key which has to be used. Neither <code>null</code> nor empty.
+   * 
+   * @return   The value associated with the property. Maybe <code>null</code>.
+   */
+  public String getString( @KNotEmpty(name="property") String property ) {
+    return properties.getProperty( property, null );
+  }
+
+  /**
+   * Changes the value for a Rectangle property.
+   * 
+   * @param property   The property key which has to be used. Neither <code>null</code> nor empty.
+   * @param value      The value which has to be saved. Maybe <code>null</code>.
+   */
+  public void setRectangle( @KNotEmpty(name="property") String property, Rectangle value ) {
+    set( Rectangle.class, property, value );
+  }
+  
+  /**
+   * Returns a value associated with a specific property key.
+   * 
+   * @param property   The property key which has to be used. Neither <code>null</code> nor empty.
+   * @param defvalue   The default value to be used if there's no value yet. Maybe <code>null</code>.
+   * 
+   * @return   The value associated with the property. Maybe <code>null</code>.
+   */
+  public Rectangle getRectangle( @KNotEmpty(name="property") String property, Rectangle defvalue ) {
+    return get( Rectangle.class, property, defvalue );
+  }
+
+  /**
+   * Returns a value associated with a specific property key.
+   * 
+   * @param property   The property key which has to be used. Neither <code>null</code> nor empty.
+   * 
+   * @return   The value associated with the property. Maybe <code>null</code>.
+   */
+  public Rectangle getRectangle( @KNotEmpty(name="property") String property ) {
+    return get( Rectangle.class, property, null );
+  }
+  
+  /**
+   * Returns a typed value of a property. 
+   *  
+   * @param type       The class type used to be stored. Not <code>null</code>.
+   * @param property   The key of the property. Neither <code>null</code> nor empty.
+   * @param defvalue   The default value which will be returned if there's no value.
+   * 
+   * @return   The typed value. Maybe <code>null</code>.
+   */
+  private <T> T get( Class<T> type, String property, T defvalue ) {
+    XmlAdapter<String,T> adapter = adapters.get( type );
+    try {
+      String result = properties.getProperty( property );
+      if( result != null ) {
+        return adapter.unmarshal( result );
+      }
+    } catch( Exception ex ) {
+      if( properties.getErrorHandler() != null ) {
+        properties.getErrorHandler().failure( this, ex.getMessage(), ex );
+      }
+    }
+    return defvalue;
+  }
+  
+  /**
+   * Changes the property value depending on the type.
+   * 
+   * @param type       The class type used to be stored. Not <code>null</code>.
+   * @param property   The key of the property. Neither <code>null</code> nor empty.
+   * @param value      The value which has to be set.
+   */
+  private <T> void set( Class<T> type, String property, T value ) {
+    XmlAdapter<String,T> adapter = adapters.get( type );
+    try {
+      String strvalue = adapter.marshal( value );
+      properties.setProperty( property, strvalue );
+    } catch( Exception ex ) {
+      if( properties.getErrorHandler() != null ) {
+        properties.getErrorHandler().failure( this, ex.getMessage(), ex );
+      }
+    }
   }
 
   /**
