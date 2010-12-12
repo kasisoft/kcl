a227c9f691f35b95f2227aa8d3f1274fa51f4b8c
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2010-12-12 13:29:32 +0000

    + added some tests for the type 'FileAdapter'

---
+++ com.kasisoft.lgpl.libs.common.test/sources/com/kasisoft/lgpl/libs/common/xml/adapters/FileAdapterTest.java
@@ -0,0 +1,55 @@
+/**
+ * Name........: FileAdapterTest
+ * Description.: Tests for the type 'FileAdapter'.
+ * Author......: Daniel Kasmeroglu
+ * E-Mail......: daniel.kasmeroglu@kasisoft.net
+ * Company.....: Kasisoft
+ * License.....: LGPL
+ */
+package com.kasisoft.lgpl.libs.common.xml.adapters;
+
+import org.testng.annotations.*;
+
+import org.testng.*;
+
+import java.io.*;
+
+/**
+ * Tests for the type 'FileAdapter'.
+ */
+@Test(groups="all")
+public class FileAdapterTest {
+
+  private FileAdapter adapter = new FileAdapter();
+
+  @DataProvider(name="createUnmarshalling")
+  public Object[][] createUnmarshalling() {
+    return new Object[][] {
+      { null                  , null                            },
+      { "testdata\\http.xsd"  , new File( "testdata/http.xsd" ) },
+      { "testdata/http.xsd"   , new File( "testdata/http.xsd" ) },
+      { "testdata\\bibo.txt"  , new File( "testdata/bibo.txt" ) },
+      { "testdata/bibo.txt"   , new File( "testdata/bibo.txt" ) },
+    };
+  }
+
+  @DataProvider(name="createMarshalling")
+  public Object[][] createMarshalling() {
+    return new Object[][] {
+      { null                              , null                },
+      { new File( "testdata/http.xsd" )   , "testdata/http.xsd" },
+      { new File( "testdata/bibo.txt" )   , "testdata/bibo.txt" },
+    };
+  }
+
+  @Test(dataProvider="createUnmarshalling")
+  public void unmarshal( String value, File expected ) throws Exception {
+    Assert.assertEquals( adapter.unmarshal( value ), expected );
+  }
+  
+  @Test(dataProvider="createMarshalling")
+  public void marshal( File value, String expected ) throws Exception {
+    Assert.assertEquals( adapter.marshal( value ), expected );
+  }
+  
+} /* ENDCLASS */
