a4f7934c4724056ffbe872023bef55c0b34979a8
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2010-12-19 12:51:39 +0000

    + added the type 'Orientation' usable to distinguish horizontal and vertical layouts + added the type 'MultiSplitPane' which allows to deal with more than two components

---
+++ com.kasisoft.lgpl.libs.common/sources/com/kasisoft/lgpl/libs/common/constants/Orientation.java
@@ -0,0 +1,36 @@
+/**
+ * Name........: Orientation
+ * Description.: Constants used to specify orientations for various purposes.
+ * Author......: Daniel Kasmeroglu
+ * E-Mail......: daniel.kasmeroglu@kasisoft.net
+ * Company.....: Kasisoft
+ * License.....: LGPL
+ */
+package com.kasisoft.lgpl.libs.common.constants;
+
+import javax.swing.*;
+
+/**
+ * Constants used to specify orientations for various purposes.
+ */
+public enum Orientation {
+  
+  Horizontal ( JSplitPane.HORIZONTAL_SPLIT ),
+  Vertical   ( JSplitPane.VERTICAL_SPLIT   );
+  
+  private int   orientation;
+  
+  Orientation( int splitpane ) {
+    orientation = splitpane;
+  }
+
+  /**
+   * Returns the orientation for a JSplitPane.
+   * 
+   * @return   The orientation for a JSplitPane.
+   */
+  public int getSplitPaneOrientation() {
+    return orientation;
+  }
+  
+} /* ENDENUM */
---
+++ com.kasisoft.lgpl.libs.common/sources/com/kasisoft/lgpl/libs/common/swing/MultiSplitPane.java
@@ -0,0 +1,85 @@
+/**
+ * Name........: MultiSplitPane
+ * Description.: A JSplitPane like variety that supports more than two parts. 
+ * Author......: Daniel Kasmeroglu
+ * E-Mail......: daniel.kasmeroglu@kasisoft.net
+ * Company.....: Kasisoft
+ * License.....: LGPL
+ */
+package com.kasisoft.lgpl.libs.common.swing;
+
+import com.kasisoft.lgpl.libs.common.constants.*;
+
+import com.kasisoft.lgpl.tools.diagnostic.*;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+/**
+ * A JSplitPane like variety that supports more than two parts.
+ */
+public class MultiSplitPane extends JPanel {
+
+  private JSplitPane[]   chain;
+  
+  public MultiSplitPane( 
+    @KNotNull(name="orientation")   Orientation   orientation, 
+    @KIRange(name="count",min=2)    int           count, 
+                                    boolean       newcontinuouslayout 
+  ) {
+    super( new BorderLayout() );
+    chain = new JSplitPane[ count - 1 ];
+    for( int i = 0; i < chain.length; i++ ) {
+      chain[i] = new JSplitPane( orientation.getSplitPaneOrientation(), newcontinuouslayout );
+    }
+    for( int i = 1; i < chain.length; i++ ) {
+      chain[ i - 1 ].setBottomComponent( chain[i] );
+    }
+    add( chain[0], BorderLayout.CENTER );
+  }
+  
+  /**
+   * Returns the number of fields available within this component.
+   * 
+   * @return   The number of fields available within this component. 
+   */
+  public int getFieldCount() {
+    return chain.length + 1;
+  }
+
+  /**
+   * Returns the component currently stored at the supplied index.
+   * 
+   * @param index   The position of the desired field. The value must be within the range {0, {@link #getFieldCount()} - 1} .
+   * 
+   * @return   The field which was stored at the desired position. Maybe <code>null</code>.
+   */
+  public Component getField( @KIPositive(name="index") int index ) {
+    if( index >= 0 ) {
+      if( index < chain.length ) {
+        return chain[ index ].getTopComponent();
+      } else if( index == chain.length ) {
+        return chain[ index - 1 ].getBottomComponent();
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Changes the component currently stored at the supplied index.
+   * 
+   * @param index       The position of the desired field. The value must be within the range {0, {@link #getFieldCount()} - 1} .
+   * @param component   The Component which has to be set. Not <code>null</code>.
+   */
+  public void setField( @KIPositive(name="index") int index, @KNotNull(name="component") Component component ) {
+    if( index >= 0 ) {
+      if( index < chain.length ) {
+        chain[ index ].setTopComponent( component );
+      } else if( index == chain.length ) {
+        chain[ index - 1 ].setBottomComponent( component );
+      }
+    }
+  }
+  
+} /* ENDCLASS */
