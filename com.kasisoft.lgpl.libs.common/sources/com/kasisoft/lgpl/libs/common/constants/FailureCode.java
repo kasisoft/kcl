7b8addae7e190d5907d3e9a124a00fa70779962d
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2011-01-26 15:38:21 +0000

    ~ each code now provides a message now

--- com.kasisoft.lgpl.libs.common/sources/com/kasisoft/lgpl/libs/common/constants/FailureCode.java
+++ com.kasisoft.lgpl.libs.common/sources/com/kasisoft/lgpl/libs/common/constants/FailureCode.java
@@ -17,45 +17,59 @@ import com.kasisoft.lgpl.tools.diagnostic.*;
 public enum FailureCode {
 
   /** everythings okay. */
-  Success                                             (   0 ),
+  Success                                             (   0, "Success"                                              ),
   
   /** an unknown failure has been launched. */
-  Unknown                                             (  -1 ),
+  Unexpected                                          (  -1, "An unexpected error occured."                         ),
   
   /** closing a resource failed. */
-  Close                                               (  -2 ),
+  Close                                               (  -2, "Closing a resource failed."                           ),
   
   /** reading or writing failed. */
-  IO                                                  (  -3 ),
+  IO                                                  (  -3, "There was an error while reading/writing a resource." ),
   
   /** skipping within a resource failed. */
-  Skip                                                (  -4 ),
+  Skip                                                (  -4, "Skipping failed for some reason."                     ),
   
   /** the encoding that has been used is not supported. */
-  InvalidEncoding                                     (  -5 ),
+  InvalidEncoding                                     (  -5, "The encoding is invalid."                             ),
 
   /** a timeout has been recognized. */
-  Timeout                                             (  -6 ),
+  Timeout                                             (  -6, "A timeout occured."                                   ),
 
   /** a listener implementation caused a failure. */
-  ListenerFailure                                     (  -7 ),
+  ListenerFailure                                     (  -7, "A listener implementation failed."                    ),
   
   /** a reflections based functionality failed. */
-  Reflections                                         (  -8 ),
+  Reflections                                         (  -8, "Error on Java reflections."                           ),
   
   /** indicates a xml related failure. */
-  XmlFailure                                          (  -9 ),
+  XmlFailure                                          (  -9, "There was an xml related error."                      ),
   
   /** a requested resource could not be found. */
-  MissingResource                                     ( -10 ),
+  MissingResource                                     ( -10, "A resource could not be found."                       ),
   
   /** a conversion failed. */
-  ConversionFailure                                   ( -11 );
-
-  private int   code;
+  ConversionFailure                                   ( -11, "Some conversion failed."                              ),
   
-  FailureCode( int value ) {
-    code = value;
+  /** a directory creation failed. */
+  CreateDirectory                                     ( -12, "The creation of a directory failed."                  );
+
+  private int      code;
+  private String   message;
+  
+  FailureCode( int value, String msg ) {
+    code    = value;
+    message = msg;
+  }
+  
+  /**
+   * Returns the message associated with this code.
+   * 
+   * @return   The message associated with this code. Neither <code>null</code> nor empty.
+   */
+  public String getMessage() {
+    return message;
   }
   
   /**
