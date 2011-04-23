d9c6114897cb27d95c7a6480981bfb3be6373640
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2011-04-23 11:02:53 +0000

    ~ added some additional tests

--- com.kasisoft.lgpl.libs.common.test/sources/com/kasisoft/lgpl/libs/common/util/StringFBufferTest.java
+++ com.kasisoft.lgpl.libs.common.test/sources/com/kasisoft/lgpl/libs/common/util/StringFBufferTest.java
@@ -18,78 +18,119 @@ import org.testng.*;
 @Test(groups="all")
 public class StringFBufferTest {
 
-  @Test
-  public void appendF() {
-    StringFBuffer buffer = new StringFBuffer();
+  @DataProvider(name="dataStringBuffers")
+  public Object[][] dataStringBuffers() {
+    return new Object[][] {
+      new Object[] { new StringFBuffer()      },
+      new Object[] { new StringFBuffer( 256 ) }
+    };
+  }
+  
+  @Test(dataProvider="dataStringBuffers")
+  public void appendF( StringFBuffer buffer ) {
     buffer.appendF( "My test is this: %s ! Not 0x%02x !", "Hello World", Integer.valueOf( 17 ) );
     Assert.assertEquals( buffer.toString(), "My test is this: Hello World ! Not 0x11 !" );
   }
   
-  @Test
-  public void charAt() {
-    StringFBuffer buffer = new StringFBuffer();
+  @Test(dataProvider="dataStringBuffers")
+  public void charAt( StringFBuffer buffer ) {
     buffer.appendF( "My test is this: %s ! Not 0x%02x !", "Hello World", Integer.valueOf( 17 ) );
     Assert.assertEquals( buffer.charAt(0)  , 'M' );
     Assert.assertEquals( buffer.charAt(-1) , '!' );
   }
 
-  @Test
-  public void substring() {
-    StringFBuffer buffer = new StringFBuffer();
+  @Test(dataProvider="dataStringBuffers")
+  public void substring( StringFBuffer buffer ) {
     buffer.appendF( "My test is this: %s ! Not 0x%02x !", "Hello World", Integer.valueOf( 17 ) );
     Assert.assertEquals( buffer.substring( 0, 2 )  , "My" );
     Assert.assertEquals( buffer.substring( -10 ) , "Not 0x11 !" );
   }
   
-  @Test
-  public void trimLeading() {
-    StringFBuffer buffer = new StringFBuffer();
+  @Test(dataProvider="dataStringBuffers")
+  public void trimLeading( StringFBuffer buffer ) {
     buffer.appendF( "\r\n   My test is this: %s ! Not 0x%02x !", "Hello World", Integer.valueOf( 17 ) );
     buffer.trimLeading();
     Assert.assertEquals( buffer.toString(), "My test is this: Hello World ! Not 0x11 !" );
   }
 
-  @Test
-  public void trimTrailing() {
-    StringFBuffer buffer = new StringFBuffer();
+  @Test(dataProvider="dataStringBuffers")
+  public void trimTrailing( StringFBuffer buffer ) {
     buffer.appendF( "My test is this: %s ! Not 0x%02x !\r\n   ", "Hello World", Integer.valueOf( 17 ) );
     buffer.trimTrailing();
     Assert.assertEquals( buffer.toString(), "My test is this: Hello World ! Not 0x11 !" );
   }
 
-  @Test
-  public void trim() {
-    StringFBuffer buffer = new StringFBuffer();
+  @Test(dataProvider="dataStringBuffers")
+  public void trim( StringFBuffer buffer ) {
     buffer.appendF( "\r\n   My test is this: %s ! Not 0x%02x !\r\n   ", "Hello World", Integer.valueOf( 17 ) );
     buffer.trim();
     Assert.assertEquals( buffer.toString(), "My test is this: Hello World ! Not 0x11 !" );
   }
   
-  @Test
-  public void endsWith() {
-    StringFBuffer buffer = new StringFBuffer();
+  @Test(dataProvider="dataStringBuffers")
+  public void endsWith( StringFBuffer buffer ) {
     buffer.appendF( "The frog is here !" );
     Assert.assertTrue  ( buffer.endsWith( true  , "here !" ) );
     Assert.assertFalse ( buffer.endsWith( true  , "HERE !" ) );
     Assert.assertTrue  ( buffer.endsWith( false , "HERE !" ) );
+    Assert.assertTrue  ( buffer.endsWith( "here !" ) );
+    Assert.assertFalse ( buffer.endsWith( "HERE !" ) );
   }
 
-  @Test
-  public void startsWith() {
-    StringFBuffer buffer = new StringFBuffer();
+  @Test(dataProvider="dataStringBuffers")
+  public void startsWith( StringFBuffer buffer ) {
     buffer.appendF( "The frog is here !" );
     Assert.assertTrue  ( buffer.startsWith( true  , "The" ) );
     Assert.assertFalse ( buffer.startsWith( true  , "THE" ) );
     Assert.assertTrue  ( buffer.startsWith( false , "THE" ) );
+    Assert.assertTrue  ( buffer.startsWith( "The" ) );
+    Assert.assertFalse ( buffer.startsWith( "THE" ) );
   }
 
-  @Test
-  public void equals() {
-    StringFBuffer buffer = new StringFBuffer();
+  @Test(dataProvider="dataStringBuffers")
+  public void equals( StringFBuffer buffer ) {
     buffer.appendF( "The frog is here !" );
-    Assert.assertTrue  ( buffer.startsWith( true  , "The frog is here !" ) );
-    Assert.assertFalse ( buffer.startsWith( true  , "THE FROG IS HERE !" ) );
-    Assert.assertTrue  ( buffer.startsWith( false , "THE FROG IS HERE !" ) );
+    Assert.assertTrue  ( buffer.equals( true  , "The frog is here !" ) );
+    Assert.assertFalse ( buffer.equals( true  , "THE FROG IS HERE !" ) );
+    Assert.assertTrue  ( buffer.equals( false , "THE FROG IS HERE !" ) );
+    Assert.assertTrue  ( buffer.equals( "The frog is here !" ) );
+    Assert.assertFalse ( buffer.equals( "THE FROG IS HERE !" ) );
+  }
+  
+  @Test(dataProvider="dataStringBuffers")
+  public void remove( StringFBuffer buffer ) {
+    buffer.appendF( "Moloko was a great band !" );
+    Assert.assertEquals( buffer.remove( "oa" ).toString(), "Mlk ws  gret bnd !" );
+  }
+
+  @Test(dataProvider="dataStringBuffers")
+  public void reverse( StringFBuffer buffer ) {
+    buffer.append( "The frog is here !" );
+    buffer.reverse();
+    Assert.assertEquals( buffer.toString(), "! ereh si gorf ehT" );
+  }
+
+  @Test(dataProvider="dataStringBuffers")
+  public void indexOf( StringFBuffer buffer ) {
+    buffer.append( "The frog is here !" );
+    Assert.assertEquals( buffer.indexOf( "frog"    ),  4 );
+    Assert.assertEquals( buffer.indexOf( "Flansch" ), -1 );
+    Assert.assertEquals( buffer.indexOf( "frog"    , 5 ), -1 );
+    Assert.assertEquals( buffer.indexOf( "Flansch" , 5 ), -1 );
+    Assert.assertEquals( buffer.indexOf( "is", "frog"    ),  4 );
+    Assert.assertEquals( buffer.indexOf( "is", "Flansch" ),  9 );
+    Assert.assertEquals( buffer.indexOf( "co", "Flansch" ), -1 );
+  }
+
+  @Test(dataProvider="dataStringBuffers")
+  public void lastIndexOf( StringFBuffer buffer ) {
+    buffer.append( "The frog is here !" );
+    Assert.assertEquals( buffer.lastIndexOf( "frog"    ),  4 );
+    Assert.assertEquals( buffer.lastIndexOf( "Flansch" ), -1 );
+    Assert.assertEquals( buffer.lastIndexOf( "frog"    , 5 ),  4 );
+    Assert.assertEquals( buffer.lastIndexOf( "Flansch" , 5 ), -1 );
+    Assert.assertEquals( buffer.lastIndexOf( "frog", "is" ),  9 );
+    Assert.assertEquals( buffer.lastIndexOf( "Flansch"    ), -1 );
   }
 
 } /* ENDCLASS */
