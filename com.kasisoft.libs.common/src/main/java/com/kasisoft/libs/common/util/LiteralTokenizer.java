394365df282d036f5175a66885f0966095286b70
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2013-01-01 16:11:26 +0000

    + simple helper class which allows to tokenize against complete literals

---
+++ com.kasisoft.libs.common/src/main/java/com/kasisoft/libs/common/util/LiteralTokenizer.java
@@ -0,0 +1,132 @@
+/**
+ * Name........: LiteralTokenizer
+ * Description.: This tokenizer operates similar to the well known StringTokenizer class with the distinction that a 
+ *               complete literal can be used for the tokenization process. 
+ * Author......: Daniel Kasmeroglu
+ * E-Mail......: daniel.kasmeroglu@kasisoft.net
+ * Company.....: Kasisoft
+ * License.....: LGPL
+ */
+package com.kasisoft.libs.common.util;
+
+import java.util.*;
+
+/**
+ * This tokenizer operates similar to the well known StringTokenizer class with the distinction that a complete literal 
+ * can be used for the tokenization process.
+ */
+public class LiteralTokenizer implements Enumeration<String> {
+
+  private int        pos;
+  private String[]   literals;
+  private String     input;
+  private boolean    doreturn;
+  private String     next;
+  
+  /**
+   * Prepares this tokenizer to operate using delimiting literals.
+   * 
+   * @param data             The String content that has to be tokenized. Not <code>null</code>.
+   * @param delimiters       A list of delimiting literals. Not <code>null</code>.
+   */
+  public LiteralTokenizer( String data, String ... delimiters ) {
+    this( data, false, delimiters );
+  }
+
+  /**
+   * Prepares this tokenizer to operate using delimiting literals.
+   * 
+   * @param data             The String content that has to be tokenized. Not <code>null</code>.
+   * @param returnliterals   <code>true</code> <=> Return delimiting literals as well.
+   * @param delimiters       A list of delimiting literals. Not <code>null</code>.
+   */
+  public LiteralTokenizer( String data, boolean returnliterals, String ... delimiters ) {
+    input         = data;
+    literals      = delimiters;
+    doreturn      = returnliterals;
+    pos           = 0;
+    next          = getNext();
+  }
+  
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean hasMoreElements() {
+    return next != null;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String nextElement() {
+    if( next == null ) {
+      return null;
+    }
+    String result = next;
+    next          = getNext();
+    return result;
+  }
+  
+  /**
+   * Returns the next literal that has to be returned by this tokenizer.
+   * 
+   * @return   The next literal that has to be returned by this tokenizer. Maybe <code>null</code>.
+   */
+  private String getNext() {
+    if( pos == -1 ) {
+      // there's no more content
+      return null;
+    }
+    String firstdelimiter = firstDelimiter();
+    int    oldpos         = pos;
+    if( firstdelimiter == null ) {
+      // there are no longer delimiting literals, so the rest becomes the next value
+      pos = -1;
+      return input.substring( oldpos );
+    }
+    int newpos = input.indexOf( firstdelimiter, pos );
+    if( newpos == pos ) {
+      // we're directly pointing to a delimiter
+      if( doreturn ) {
+        // the user wants to get the delimiting literal
+        newpos = pos + firstdelimiter.length();
+        pos    = newpos;
+        if( pos >= input.length() ) {
+          pos = -1;
+        }
+        return input.substring( oldpos, newpos );
+      } else {
+        // the user wants to skip delimiting literals, so try to get the next literal after the delimiter
+        pos = pos + firstdelimiter.length();
+        if( pos >= input.length() ) {
+          pos = -1;
+        }
+        return getNext();
+      }
+    } else {
+      pos = newpos;
+      return input.substring( oldpos, newpos );
+    }
+  }
+  
+  /**
+   * Returns the delimiting literal that will be detected first.
+   * 
+   * @return   The delimiting literal that will be detected first. Maybe <code>null</code>.
+   */
+  private String firstDelimiter() {
+    String result = null;
+    int    next   = Integer.MAX_VALUE;
+    for( String literal : literals ) {
+      int newnext = input.indexOf( literal, pos );
+      if( (newnext < next) && (newnext >= pos) ) {
+        next    = newnext;
+        result  = literal;
+      }
+    }
+    return( result );
+  }
+
+} /* ENDCLASS */
---
+++ com.kasisoft.libs.common/src/test/java/com/kasisoft/libs/common/util/LiteralTokenizerTest.java
@@ -0,0 +1,63 @@
+/**
+ * Name........: LiteralTokenizerTest
+ * Description.: Testcases for the class 'LiteralTokenizer'. 
+ * Author......: Daniel Kasmeroglu
+ * E-Mail......: daniel.kasmeroglu@kasisoft.net
+ * Company.....: Kasisoft
+ * License.....: LGPL
+ */
+package com.kasisoft.libs.common.util;
+
+import org.testng.annotations.*;
+
+import org.testng.*;
+
+import java.util.*;
+
+/**
+ * Testcases for the class 'LiteralTokenizer'.
+ */
+@Test(groups="all")
+public class LiteralTokenizerTest {
+
+  @DataProvider(name="tokenizeDataProvider")
+  public Object[][] tokenizeDataProvider() {
+    
+    String input1 = "@PART@ ist @FLUPPE@ heute hier";
+    String input2 = "Fred ist @FLUPPE@ heute @PART@";
+    String input3 = "Fred @PART@ @FLUPPE@ heute hier";
+    String input4 = "#PART# ist #FLUPPE# heute hier";
+    String input5 = "";
+    
+    return new Object[][] {
+        
+      { input1, Boolean.FALSE , new String[] { " ist ", " heute hier" } },
+      { input1, Boolean.TRUE  , new String[] { "@PART@", " ist ", "@FLUPPE@", " heute hier" } },
+      
+      { input2, Boolean.FALSE , new String[] { "Fred ist ", " heute " } },
+      { input2, Boolean.TRUE  , new String[] { "Fred ist ", "@FLUPPE@", " heute ", "@PART@" } },
+
+      { input3, Boolean.FALSE , new String[] { "Fred ", " ", " heute hier" } },
+      { input3, Boolean.TRUE  , new String[] { "Fred ", "@PART@", " ", "@FLUPPE@", " heute hier" } },
+
+      { input4, Boolean.FALSE , new String[] { "#PART# ist #FLUPPE# heute hier" } },
+      { input4, Boolean.TRUE  , new String[] { "#PART# ist #FLUPPE# heute hier" } },
+
+      { input5, Boolean.FALSE , new String[] { "" } },
+      { input5, Boolean.TRUE  , new String[] { "" } },
+
+    };
+    
+  }
+  
+  @Test(dataProvider="tokenizeDataProvider")
+  public void tokenize( String input, boolean returndelimiters, String[] expected ) {
+    LiteralTokenizer tokenizer = new LiteralTokenizer( input, returndelimiters, "@PART@", "@FLUPPE@" );
+    List<String>     tokens    = new ArrayList<String>();
+    while( tokenizer.hasMoreElements() ) {
+      tokens.add( tokenizer.nextElement() );
+    }
+    Assert.assertEquals( tokens.toArray(), expected );
+  }
+
+} /* ENDCLASS */
