7bc2f369dcc6b8eef8541c2ce018d95a078dd5af
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2014-12-07 15:20:46 +0100

    + a dispatcher for 'ComponentEvent'

---
+++ com.kasisoft.libs.common/src/main/java/com/kasisoft/libs/common/ui/event/ComponentEventDispatcher.java
@@ -0,0 +1,25 @@
+package com.kasisoft.libs.common.ui.event;
+
+import java.awt.event.*;
+
+import lombok.*;
+
+/**
+ * Basic implementation of a dispatcher for ComponentEvent's. Listeners called using this dispatcher will be executed 
+ * within the EventDispatchThread.
+ * 
+ * @author daniel.kasmeroglu@kasisoft.net
+ */
+public class ComponentEventDispatcher extends AbstractEventDispatcher<ComponentListener,ComponentEvent>  {
+
+  @Override
+  protected void invokeEvent( @NonNull ComponentListener listener, @NonNull ComponentEvent event ) {
+    switch( event.getID() ) {
+    case ComponentEvent.COMPONENT_HIDDEN  : listener.componentHidden  ( event ); break;
+    case ComponentEvent.COMPONENT_MOVED   : listener.componentMoved   ( event ); break;
+    case ComponentEvent.COMPONENT_RESIZED : listener.componentResized ( event ); break;
+    case ComponentEvent.COMPONENT_SHOWN   : listener.componentShown   ( event ); break;
+    }
+  }
+
+} /* ENDCLASS */
