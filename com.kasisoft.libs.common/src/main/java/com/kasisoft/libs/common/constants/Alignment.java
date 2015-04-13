25f0dd42bf175c481faf7c9a19544a709df34a73
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2015-04-14 01:12:39 +0200

    ~ added values for a 'TitledBorder' instance

--- com.kasisoft.libs.common/src/main/java/com/kasisoft/libs/common/constants/Alignment.java
+++ com.kasisoft.libs.common/src/main/java/com/kasisoft/libs/common/constants/Alignment.java
@@ -1,6 +1,7 @@
 package com.kasisoft.libs.common.constants;
 
 import javax.swing.*;
+import javax.swing.border.*;
 
 import lombok.*;
 import lombok.experimental.*;
@@ -13,19 +14,22 @@ import lombok.experimental.*;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public enum Alignment {
 
-  Left   ( SwingConstants.LEFT   ) ,
-  Center ( SwingConstants.CENTER ) ,
-  Right  ( SwingConstants.RIGHT  ) ;
+  Left   ( SwingConstants.LEFT   , TitledBorder.LEFT    ) ,
+  Center ( SwingConstants.CENTER , TitledBorder.CENTER  ) ,
+  Right  ( SwingConstants.RIGHT  , TitledBorder.RIGHT   ) ;
   
-  @Getter int  alignmentX;
+  @Getter int   alignmentX;
+  @Getter int   titledBorderX;
   
   /**
    * Sets up this constant with an alignment value that is used for swing components. 
    * 
-   * @param value   An alignment value that is used for swing components.
+   * @param value     An alignment value that is used for swing components.
+   * @param borderx   The alignment for the titled border.
    */
-  Alignment( int value ) {
-    alignmentX = value;
+  Alignment( int value, int borderx ) {
+    alignmentX    = value;
+    titledBorderX = borderx;
   }
   
 } /* ENDENUM */
