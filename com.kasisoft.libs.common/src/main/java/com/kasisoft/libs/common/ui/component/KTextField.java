d3e4e0262731b90adf7ea4403f37ee76e04dc512
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2015-04-14 01:13:58 +0200

    ~ added a placeholder

--- com.kasisoft.libs.common/src/main/java/com/kasisoft/libs/common/ui/component/KTextField.java
+++ com.kasisoft.libs.common/src/main/java/com/kasisoft/libs/common/ui/component/KTextField.java
@@ -6,16 +6,34 @@ import javax.swing.*;
 
 import java.awt.event.*;
 
+import java.awt.*;
+
+import lombok.*;
+import lombok.experimental.*;
+
 /**
+ * Initializes this component.
+ * 
  * @author daniel.kasmeroglu@kasisoft.net
  */
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class KTextField extends JTextField {
 
+  static final Color DEFAULT_PLACEHOLDER = Color.lightGray;
+  
+  @Getter @Setter
+  String   placeHolder;
+  
+  @Getter @Setter
+  Color    placeHolderColor;
+  
   /**
    * Sets up this text field.
    */
   public KTextField() {
     super();
+    setColumns(6);
+    placeHolderColor  = DEFAULT_PLACEHOLDER;
   }
   
   /**
@@ -25,6 +43,8 @@ public class KTextField extends JTextField {
    */
   public KTextField( String content ) {
     super( content );
+    setColumns(6);
+    placeHolderColor  = DEFAULT_PLACEHOLDER;
   }
 
   @Override
@@ -33,6 +53,20 @@ public class KTextField extends JTextField {
       ((WSListener) l).configure( this );
     }
     super.addFocusListener( l );
+  }
+
+  @Override
+  protected void paintComponent( Graphics g ) {
+    super.paintComponent( g );
+    if( placeHolder != null ) {
+      if( getText().isEmpty() ) {
+        Graphics2D  g2d = (Graphics2D) g.create();
+        FontMetrics fm  = g2d.getFontMetrics();
+        g2d.setColor( placeHolderColor != null ? placeHolderColor : DEFAULT_PLACEHOLDER );
+        g2d.drawString( placeHolder, 2, fm.getHeight() - 1 );
+        g2d.dispose();
+      }
+    }
   }
 
 } /* ENDCLASS */
