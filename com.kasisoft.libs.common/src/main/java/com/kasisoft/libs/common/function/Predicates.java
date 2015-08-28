5018bbb961d02e38605e297c8daff92f918471c6
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2015-08-29 00:45:43 +0200

    + collection of useful predicates

---
+++ com.kasisoft.libs.common/src/main/java/com/kasisoft/libs/common/function/Predicates.java
@@ -0,0 +1,44 @@
+package com.kasisoft.libs.common.function;
+
+import java.util.function.*;
+
+import java.util.regex.*;
+
+/**
+ * Collection of predicates.
+ * 
+ * @author daniel.kasmeroglu@kasisoft.net
+ */
+public class Predicates {
+  
+  public static final Predicate<?> ACCEPT_ALL = $ -> true;
+  
+  public static final Predicate<?> ACCEPT_NONE = $ -> false;
+  
+  public static final Predicate<String> IS_JAVA_CLASS_FILE = new IsJavaClassFile();
+
+  public static final Predicate<String> IS_INNER_JAVA_CLASS_FILE = new IsInnerJavaClassFile();
+  
+  private static class IsJavaClassFile implements Predicate<String> {
+
+    Pattern pattern = Pattern.compile( "^(.+)(.class)$" );
+    
+    @Override
+    public boolean test( String resource ) {
+      return pattern.matcher( resource ).matches();
+    }
+    
+  } /* ENDCLASS */
+
+  private static class IsInnerJavaClassFile implements Predicate<String> {
+
+    Pattern pattern = Pattern.compile( "^([^$]+)\\$(.*)(.class)$" );
+    
+    @Override
+    public boolean test( String resource ) {
+      return pattern.matcher( resource ).matches();
+    }
+    
+  } /* ENDCLASS */
+  
+} /* ENDCLASS */
---
+++ com.kasisoft.libs.common/src/test/java/com/kasisoft/libs/common/function/PredicatesTest.java
@@ -0,0 +1,56 @@
+package com.kasisoft.libs.common.function;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+import org.testng.annotations.*;
+
+/**
+ * Tests for 'Predicates'.
+ * 
+ * @author daniel.kasmeroglu@kasisoft.net
+ */
+public class PredicatesTest {
+
+  @DataProvider(name="isJavaClassFile")
+  public Object[][] isJavaClassFileData() {
+    return new Object[][] {
+      { "com/sample/Bibo.class"           , true  },
+      { "Bibo.class"                      , true  },
+      { "com/sample/Bibo$1.class"         , true  },
+      { "Bibo$1.class"                    , true  },
+      { "com/sample/Bibo$Sample.class"    , true  },
+      { "Bibo$Sample.class"               , true  },
+      { "com.sample.Bibo"                 , false },
+      { "Bibo"                            , false },
+      { ""                                , false },
+    };
+  }
+  
+  @Test(dataProvider = "isJavaClassFile")
+  public void isJavaClassFile( String classname, boolean expected ) {
+    assertThat( Predicates.IS_JAVA_CLASS_FILE.test( classname ), is( expected ) );
+  }
+
+  
+  @DataProvider(name="isInnerJavaClassFile")
+  public Object[][] isInnerJavaClassFileData() {
+    return new Object[][] {
+      { "com/sample/Bibo.class"           , false },
+      { "Bibo.class"                      , false },
+      { "com/sample/Bibo$1.class"         , true  },
+      { "Bibo$1.class"                    , true  },
+      { "com/sample/Bibo$Sample.class"    , true  },
+      { "Bibo$Sample.class"               , true  },
+      { "com.sample.Bibo"                 , false },
+      { "Bibo"                            , false },
+      { ""                                , false },
+    };
+  }
+  
+  @Test(dataProvider = "isInnerJavaClassFile")
+  public void isInnerJavaClassFile( String classname, boolean expected ) {
+    assertThat( Predicates.IS_INNER_JAVA_CLASS_FILE.test( classname ), is( expected ) );
+  }
+
+}
