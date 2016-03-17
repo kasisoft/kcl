6559a88497f101b7b41fd2a37d0fb8efae615f34
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2016-03-17 23:19:46 +0100

    + added several helper functions allowing to make use of recent java 8 features

--- com.kasisoft.libs.common/src/main/java/com/kasisoft/libs/common/base/FailureException.java
+++ com.kasisoft.libs.common/src/main/java/com/kasisoft/libs/common/base/FailureException.java
@@ -4,6 +4,8 @@ import lombok.experimental.*;
 
 import lombok.*;
 
+import java.util.function.*;
+
 /**
  * Specialisation of the RuntimeException which provides a numerical code which allows to handle this exception in a 
  * more apropriate way than checking it's message.
@@ -13,6 +15,8 @@ import lombok.*;
 @Getter @FieldDefaults(level = AccessLevel.PRIVATE)
 public class FailureException extends RuntimeException {
 
+  static final Object[] NO_PARAMS = new Object[0];
+  
   FailureCode   failurecode;
   Object[]      params;
 
@@ -20,6 +24,74 @@ public class FailureException extends RuntimeException {
     super( message, cause );
     failurecode = code;
     params      = parameters;
+  }
+  
+  public static FailureException wrap( Exception ex ) {
+    if( ex instanceof FailureException ) {
+      return (FailureException) ex;
+    } else {
+      return new FailureException( ex.getLocalizedMessage(), FailureCode.Unexpected, ex, NO_PARAMS );
+    }
+  }
+
+  public static <T> Consumer<T> ensure( Consumer<T> consumer ) {
+    return new Consumer<T>() {
+
+      @Override
+      public void accept( T t ) {
+        try {
+          consumer.accept(t);
+        } catch( Exception ex ) {
+          throw wrap(ex);
+        }
+      }
+
+    };
+  }
+
+  public static <T1,T2> BiConsumer<T1,T2> ensure( BiConsumer<T1,T2> consumer ) {
+    return new BiConsumer<T1,T2>() {
+
+      @Override
+      public void accept( T1 t1, T2 t2 ) {
+        try {
+          consumer.accept( t1, t2 );
+        } catch( Exception ex ) {
+          throw wrap(ex);
+        }
+      }
+
+    };
+  }
+
+  public static <S,T> Function<S,T> ensure( Function<S,T> function ) {
+    return new Function<S, T>(){
+
+      @Override
+      public T apply( S s ) {
+        try {
+          return function.apply( s );
+        } catch( Exception ex ) {
+          throw wrap( ex );
+        }
+      }
+
+    };
+  }
+
+  public static <S,T,R> BiFunction<S,T,R> ensure( BiFunction<S,T,R> function ) {
+    return new BiFunction<S,T,R>() {
+
+      @Override
+      public R apply( S s, T t ) {
+        try {
+          return function.apply( s, t );
+        } catch( Exception ex ) {
+          throw wrap( ex );
+        }
+      }
+
+    };
   }
   
 } /* ENDCLASS */
