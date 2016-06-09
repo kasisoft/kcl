01fa978f5287596566ca1d77c12e2be28cba23e7
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2016-06-10 00:58:49 +0200

    + added test cases for the class 'Blacklist'

--- com.kasisoft.libs.common/src/main/java/com/kasisoft/libs/common/util/Blacklist.java
+++ com.kasisoft.libs.common/src/main/java/com/kasisoft/libs/common/util/Blacklist.java
@@ -19,14 +19,14 @@ import java.nio.file.*;
 import java.io.*;
 
 /**
- * A simple helper allowing to manage blacklists.
+ * A simple helper allowing to manage blacklists. It obviously can be used as a whitelist. 
  * 
  * @author daniel.kasmeroglu@kasisoft.net
  */
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @ToString(of = {"list", "commentPrefix"})
 @EqualsAndHashCode(of = {"list", "commentPrefix"})
-public class Blacklist implements Predicate<String>, Function<CharSequence,StringBuilder> {
+public class Blacklist implements Predicate<String> {
   
   static final Comparator<String> LENGTH_COMPARATOR = new Comparator<String>() {
 
@@ -38,7 +38,7 @@ public class Blacklist implements Predicate<String>, Function<CharSequence,Strin
       if( result == 0 ) {
         result = o1.compareTo( o2 );
       }
-      return result;
+      return -result;
     }
     
   };
@@ -65,7 +65,7 @@ public class Blacklist implements Predicate<String>, Function<CharSequence,Strin
    */
   public Blacklist( URL url ) {
     this();
-    IoFunctions.forReaderDo( url, this::load );
+    load( url );
   }
 
   /**
@@ -75,12 +75,7 @@ public class Blacklist implements Predicate<String>, Function<CharSequence,Strin
    */
   public Blacklist( String classpath ) {
     this();
-    ClassLoader cl = Thread.currentThread().getContextClassLoader();
-    if( cl == null ) {
-      cl = getClass().getClassLoader();
-    }
-    URL url = cl.getResource( classpath );
-    IoFunctions.forReaderDo( url, this::load );
+    load( classpath );
   }
 
   /**
@@ -90,19 +85,32 @@ public class Blacklist implements Predicate<String>, Function<CharSequence,Strin
    */
   public Blacklist( Path path ) {
     this();
-    IoFunctions.forReaderDo( path, this::load );
+    load( path );
+  }
+  
+  /**
+   * Returns an unmodifiable list of blacklist entries.
+   * 
+   * @return   An unmodifiable list of blacklist entries. Not <code>null</code>.
+   */
+  public List<String> getBlacklisted() {
+    return Collections.unmodifiableList( list );
   }
   
   /**
    * Adds the supplied value to the black list.
    * 
-   * @param blacklist   The new value to be used for black listing. Neither <code>null</code> nor empty. 
+   * @param blacklisted   The new value to be used for black listing. Neither <code>null</code> nor empty. 
    */
-  public void add( @NonNull String blacklist ) {
-    String value = StringFunctions.cleanup( blacklist );
+  public void add( @NonNull String blacklisted ) {
+    String value = StringFunctions.cleanup( blacklisted );
     if( value != null ) {
-      list.add( value );
-      lowercaseList.add( value.toLowerCase() );
+      int idx = Collections.binarySearch( list, value, LENGTH_COMPARATOR );
+      if( idx < 0 ) {
+        idx = -idx - 1;
+        list.add( idx, value );
+        lowercaseList.add( idx, value.toLowerCase() );
+      }
     }
   }
 
@@ -130,14 +138,42 @@ public class Blacklist implements Predicate<String>, Function<CharSequence,Strin
   /**
    * Returns the currently used comment prefix.
    * 
-   * @return   The currently used comment prefix. Neither <code>null</code> nor empty.
+   * @return   The currently used comment prefix. Not blank.
    */
   public String getCommentPrefix() {
     return commentPrefix;
   }
+
+  /**
+   * Loads the blacklist from the supplied url.
+   * 
+   * @param url   The URL pointing to the blacklist. Not <code>null</code>.
+   */
+  public synchronized void load( @NonNull URL url ) {
+    IoFunctions.forReaderDo( url, this::load );
+  }
+
+  /**
+   * Loads the blacklist from a resource on the classpath.
+   * 
+   * @param resource   A blacklist resource on the classpath. Not blank.
+   */
+  public synchronized void load( @NonNull String resource ) {
+    URL url = MiscFunctions.getResource( getClass(), resource );
+    IoFunctions.forReaderDo( url, this::load );
+  }
   
   /**
-   * Loads the blacklist supplied by the supplied reader.
+   * Loads the blacklist from a specific filesystem location.
+   * 
+   * @param path   The filesystem location of the blacklist. Not <code>null</code>.
+   */
+  public synchronized void load( @NonNull Path path ) {
+    IoFunctions.forReaderDo( path, this::load );
+  }
+  
+  /**
+   * Loads the blacklist supplied by the reader.
    * 
    * @param reader   The reader providing the blacklisted content. Not <code>null</code>.
    */
@@ -167,7 +203,47 @@ public class Blacklist implements Predicate<String>, Function<CharSequence,Strin
   }
 
   /**
-   * Returns a startsWith test. 
+   * Returns <code>true</code> if the supplied literal is blacklisted.
+   * 
+   * @param t   The text that shall be tested. Maybe <code>null</code>.
+   * 
+   * @return   <code>true</code> <=> The supplied text is blacklisted.
+   */
+  @Override
+  public synchronized boolean test( String t ) {
+    if( t != null ) {
+      return list.contains(t);
+    }
+    return false;
+  }
+  
+  /**
+   * Returns a predicate like {@link #test(String)} with the difference that the test ignores case sensitivity.
+   * 
+   * @return   A test ignoring case sensitivity. Not <code>null</code>.
+   */
+  public Predicate<String> testIgnoreCase() {
+    return this::testCI;
+  }
+
+  /**
+   * Implements the predicate returned by {@link #testIgnoreCase()}.
+   * 
+   * @param t   The text that is supposed to be tested. Maybe <code>null</code>.
+   * 
+   * @return   <code>true</code> <=> The supplied text is blacklisted.
+   */
+  private synchronized boolean testCI( String t ) {
+    if( t != null ) {
+      String candidate = t.toLowerCase();
+      return lowercaseList.contains( candidate );
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * Returns a startsWith test which checks whether a text starts with a blacklisted literal.
    * 
    * @param ignorecase   <code>true</code> <=> Ignore case.
    * 
@@ -178,7 +254,7 @@ public class Blacklist implements Predicate<String>, Function<CharSequence,Strin
   }
 
   /**
-   * Returns a startsWith test (case sensitive) 
+   * Returns a startsWith test which checks whether a text starts with a blacklisted literal (case sensitive).
    * 
    * @return   A startsWith test for this blacklist. Not <code>null</code>.
    */
@@ -187,7 +263,36 @@ public class Blacklist implements Predicate<String>, Function<CharSequence,Strin
   }
   
   /**
-   * Returns an endsWith test. 
+   * Implements the predicate returned by {@link #startsWith(boolean)} (case sensitive).
+   * 
+   * @param t   The text that is supposed to be tested. Maybe <code>null</code>.
+   * 
+   * @return   <code>true</code> <=> The supplied text starts with a blacklisted literal.
+   */
+  private synchronized boolean testStartsWithCS( String t ) {
+    if( t != null ) {
+      return test( list, t::startsWith );
+    }
+    return false;
+  }
+
+  /**
+   * Implements the predicate returned by {@link #startsWith(boolean)} (case insensitive).
+   * 
+   * @param t   The text that is supposed to be tested. Maybe <code>null</code>.
+   * 
+   * @return   <code>true</code> <=> The supplied text starts with a blacklisted literal.
+   */
+  private synchronized boolean testStartsWithCI( String t ) {
+    if( t != null ) {
+      String candidate = t.toLowerCase();
+      return test( lowercaseList, candidate::startsWith );
+    }
+    return false;
+  }
+
+  /**
+   * Returns an endsWith test which checks whether a text ends with a blacklisted literal.  
    * 
    * @param ignorecase   <code>true</code> <=> Ignore case.
    * 
@@ -198,7 +303,7 @@ public class Blacklist implements Predicate<String>, Function<CharSequence,Strin
   }
   
   /**
-   * Returns an endsWith test. 
+   * Returns an endsWith test which checks whether a text ends with a blacklisted literal  (case sensitive).  
    * 
    * @return   A endsWith test for this blacklist. Not <code>null</code>.
    */
@@ -207,63 +312,92 @@ public class Blacklist implements Predicate<String>, Function<CharSequence,Strin
   }
 
   /**
-   * Returns a test ignoring case sensitivity.
+   * Implements the predicate returned by {@link #endsWith(boolean)} (case sensitive).
    * 
-   * @return   A test ignoring case sensitivity. Not <code>null</code>.
+   * @param t   The text that is supposed to be tested. Maybe <code>null</code>.
+   * 
+   * @return   <code>true</code> <=> The supplied text ends with a blacklisted literal.
    */
-  public Predicate<String> testIgnoreCase() {
-    return this::testCI;
-  }
-  
-  @Override
-  public synchronized boolean test( String t ) {
-    if( t != null ) {
-      return list.contains(t);
-    } else {
-      return false;
-    }
-  }
-  
-  private synchronized boolean testCI( String t ) {
-    if( t != null ) {
-      String candidate = t.toLowerCase();
-      return lowercaseList.contains( candidate );
-    } else {
-      return false;
-    }
-  }
-
-  private synchronized boolean testStartsWithCS( String t ) {
-    if( t != null ) {
-      return test( list, t, t::startsWith );
-    }
-    return false;
-  }
-
-  private synchronized boolean testStartsWithCI( String t ) {
-    if( t != null ) {
-      String candidate = t.toLowerCase();
-      return test( lowercaseList, candidate, candidate::startsWith );
-    }
-    return false;
-  }
-
   private synchronized boolean testEndsWithCS( String t ) {
     if( t != null ) {
-      return test( list, t, t::endsWith );
+      return test( list, t::endsWith );
     }
     return false;
   }
 
+  /**
+   * Implements the predicate returned by {@link #endsWith(boolean)} (case insensitive).
+   * 
+   * @param t   The text that is supposed to be tested. Maybe <code>null</code>.
+   * 
+   * @return   <code>true</code> <=> The supplied text ends with a blacklisted literal.
+   */
   private synchronized boolean testEndsWithCI( String t ) {
     if( t != null ) {
       String candidate = t.toLowerCase();
-      return test( lowercaseList, candidate, candidate::endsWith );
+      return test( lowercaseList, candidate::endsWith );
     }
     return false;
   }
-  
-  private boolean test( List<String> list, String candidate, Predicate<String> test ) {
+
+  /**
+   * Returns a contains test which checks whether a text contains a blacklisted literal.
+   * 
+   * @param ignorecase   <code>true</code> <=> Ignore case.
+   * 
+   * @return   A contains test for this blacklist. Not <code>null</code>.
+   */
+  public Predicate<String> contains( boolean ignorecase ) {
+    return ignorecase ? this::testContainsCI : this::testContainsCS;
+  }
+
+  /**
+   * Returns a contains test which checks whether a text contains a blacklisted literal.
+   * 
+   * @return   A contains test for this blacklist. Not <code>null</code>.
+   */
+  public Predicate<String> contains() {
+    return contains( false );
+  }
+
+  /**
+   * Implements the predicate returned by {@link #contains(boolean)} (case sensitive).
+   * 
+   * @param t   The text that is supposed to be tested. Maybe <code>null</code>.
+   * 
+   * @return   <code>true</code> <=> The supplied text contains a blacklisted literal.
+   */
+  private synchronized boolean testContainsCS( String t ) {
+    if( t != null ) {
+      return test( list, t::contains );
+    }
+    return false;
+  }
+
+  /**
+   * Implements the predicate returned by {@link #contains(boolean)} (case insensitive).
+   * 
+   * @param t   The text that is supposed to be tested. Maybe <code>null</code>.
+   * 
+   * @return   <code>true</code> <=> The supplied text contains a blacklisted literal.
+   */
+  private synchronized boolean testContainsCI( String t ) {
+    if( t != null ) {
+      String candidate = t.toLowerCase();
+      return test( lowercaseList, candidate::contains );
+    }
+    return false;
+  }
+
+  /**
+   * Returns <code>true</code> if the supplied predicate matches for at least one list entry.
+   * 
+   * @param list   The list entries that might be matched. Not <code>null</code>.
+   * @param test   The predicate used for the test. Not <code>null</code>.
+   * 
+   * @return   <code>true</code> <=> The predicate matched one entry.
+   */
+  private boolean test( List<String> list, Predicate<String> test ) {
     for( String element : list ) {
       if( test.test( element ) ) {
         return true;
@@ -272,22 +406,141 @@ public class Blacklist implements Predicate<String>, Function<CharSequence,Strin
     return false;
   }
 
-  @Override
-  public synchronized StringBuilder apply( CharSequence t ) {
+  /**
+   * Returns a function that converts a CharSequence into a StringBuilder while dropping the blacklisted portions.
+   * This function is case sensitive.
+   * 
+   * @return   A function that converts a CharSequence into a StringBuilder while dropping the blacklisted portions. 
+   *           Not <code>null</code>.
+   */
+  public <T extends CharSequence> Function<T,StringBuilder> cleanup() {
+    return cleanup( false );
+  }
+
+  /**
+   * Returns a function that converts a CharSequence into a StringBuilder while dropping the blacklisted portions.
+   *
+   * @param ignorecase   <code>true</code> <=> Ignore case sensitivity.
+   * 
+   * @return   A function that converts a CharSequence into a StringBuilder while dropping the blacklisted portions. 
+   *           Not <code>null</code>.
+   */
+  public <T extends CharSequence> Function<T,StringBuilder> cleanup( boolean ignorecase ) {
+    return cleanup( ignorecase, null );
+  }
+
+  /**
+   * Returns a function that converts a CharSequence into a StringBuilder while dropping the blacklisted portions.
+   * This function is case sensitive.
+   * 
+   * @param statistic   This Consumer instance allows to react on each detected blacklisted literal. 
+   *                    Maybe <code>null</code>.
+   * 
+   * @return   A function that converts a CharSequence into a StringBuilder while dropping the blacklisted portions. 
+   *           Not <code>null</code>.
+   */
+  public <T extends CharSequence> Function<T,StringBuilder> cleanup( Consumer<String> statistic ) {
+    return cleanup( false, statistic );
+  }
+  
+  /**
+   * Returns a function that converts a CharSequence into a StringBuilder while dropping the blacklisted portions.
+   * 
+   * @param ignorecase   <code>true</code> <=> Ignore case sensitivity.
+   * @param statistic    This Consumer instance allows to react on each detected blacklisted literal. 
+   *                     Maybe <code>null</code>.
+   * 
+   * @return   A function that converts a CharSequence into a StringBuilder while dropping the blacklisted portions. 
+   *           Not <code>null</code>.
+   */
+  public <T extends CharSequence> Function<T,StringBuilder> cleanup( boolean ignorecase, Consumer<String> statistic ) {
+    return ignorecase ? $ -> applyCI( $, statistic ) : $ -> applyCS( $, statistic );
+  }
+
+  /**
+   * Drops all black listed elements from the supplied sequence (case insensitive).
+   * 
+   * @param t           The text that shall be freed from blacklisted elements. Maybe <code>null</code>.
+   * @param statistic   An optional Consumer to be notified when a blacklisted element had been detected.
+   *                    Maybe <code>null</code>.
+   * 
+   * @return   A StringBuilder instance providing the cleansed text. Not <code>null</code> and may be altered 
+   *           by the caller.
+   */
+  private synchronized StringBuilder applyCI( CharSequence t, Consumer<String> statistic ) {
     StringBuilder result = new StringBuilder();
-    result.append(t);
-    for( String entry : list ) {
-      removeAll( result, entry );
+    if( t != null ) {
+      if( statistic == null ) {
+        statistic = this::noStatistic;
+      }
+      result.append(t);
+      StringBuilder lower = new StringBuilder( t.toString().toLowerCase() );
+      for( String entry : lowercaseList ) {
+        removeAll( lower, result, entry, statistic );
+      }
     }
     return result;
   }
-  
-  private void removeAll( StringBuilder builder, String literal ) {
-    int idx = builder.indexOf( literal );
-    while( idx != -1 ) {
-      builder.delete( idx, idx + literal.length() );
-      idx = builder.indexOf( literal );
+
+  /**
+   * Drops all black listed elements from the supplied sequence (case sensitive).
+   * 
+   * @param t           The text that shall be freed from blacklisted elements. Maybe <code>null</code>.
+   * @param statistic   An optional Consumer to be notified when a blacklisted element had been detected.
+   *                    Maybe <code>null</code>.
+   * 
+   * @return   A StringBuilder instance providing the cleansed text. Not <code>null</code> and may be altered 
+   *           by the caller.
+   */
+  private synchronized StringBuilder applyCS( CharSequence t, Consumer<String> statistic ) {
+    StringBuilder result = new StringBuilder();
+    if( t != null ) {
+      if( statistic == null ) {
+        statistic = this::noStatistic;
+      }
+      result.append(t);
+      for( String entry : list ) {
+        removeAll( result, null, entry, statistic );
+      }
     }
+    return result;
+  }
+
+  /**
+   * Removes all occurrences of a literal within the supplied StringBuilder instances.
+   *
+   * @param builder1   The StringBuilder instance which contains the textual representation used to locate
+   *                   the literals. Not <code>null</code>.
+   * @param builder2   The StringBuilder instance which will be altered accordingly. Maybe <code>null</code>.
+   * @param statistic   An optional Consumer to be notified when a blacklisted element had been detected.
+   */
+  private void removeAll( StringBuilder builder1, StringBuilder builder2, String literal, Consumer<String> statistic ) {
+    int idx = builder1.lastIndexOf( literal );
+    while( idx != -1 ) {
+      remover( builder1, builder2, idx, literal, statistic );
+      idx = builder1.lastIndexOf( literal );
+    }
+  }
+
+  /**
+   * Removes one occurrence of a literal.
+   * 
+   * @param builder1    The StringBuilder instance which contains the textual representation used to locate
+   *                    the literals. Not <code>null</code>.
+   * @param builder2    The StringBuilder instance which will be altered accordingly. Maybe <code>null</code>.
+   * @param idx         The index where the literal had been located.
+   * @param literal     The literal that had been detected. Not blank.
+   * @param statistic   An optional Consumer to be notified when a blacklisted element had been detected.
+   */
+  private void remover( StringBuilder builder1, StringBuilder builder2, int idx, String literal, Consumer<String> statistic ) {
+    builder1.delete( idx, idx + literal.length() );
+    if( builder2 != null ) {
+      builder2.delete( idx, idx + literal.length() );
+    }
+    statistic.accept( literal );
+  }
+  
+  private void noStatistic( String literal ) {
   }
   
 } /* ENDCLASS */
---
+++ com.kasisoft.libs.common/src/test/java/com/kasisoft/libs/common/util/BlacklistTest.java
@@ -0,0 +1,171 @@
+package com.kasisoft.libs.common.util;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static org.testng.Assert.*;
+
+import org.testng.annotations.*;
+
+import com.kasisoft.libs.common.io.*;
+
+import java.util.*;
+
+/**
+ * Testcases for the class 'ArrayFunctions'.
+ * 
+ * @author daniel.kasmeroglu@kasisoft.net
+ */
+public class BlacklistTest {
+
+  Blacklist   csblacklist;
+  Blacklist   ciblacklist;
+  String      expected;
+  
+  @BeforeTest
+  public void setUp() {
+    
+    expected    = IoFunctions.readTextFully( MiscFunctions.getResource( "/blacklist/text_base.txt" ) );
+    assertNotNull( expected );
+    
+    csblacklist = new Blacklist( "/blacklist/blacklist_01.txt" );
+    ciblacklist = new Blacklist( "/blacklist/blacklist_02.txt" );
+
+  }
+  
+  @Test(groups="all")
+  public void commentPrefix() {
+    
+    Blacklist prefixed = new Blacklist();
+    prefixed.setCommentPrefix( "$%" );
+    prefixed.load( "/blacklist/blacklist_03.txt" );
+    
+    List<String> original = csblacklist.getBlacklisted();
+    List<String> other    = prefixed.getBlacklisted();
+    assertNotNull( original );
+    assertNotNull( other    );
+    assertThat( other, is( original ) );
+  }
+  
+  @Test(groups="all")
+  public void simpleCaseSensitiveMatch() {
+    assertTrue( csblacklist.test( "blacklisted" ) );
+  }
+
+  @Test(groups="all")
+  public void simpleCaseInsensitiveMatch() {
+    assertTrue( csblacklist.testIgnoreCase().test( "blackLISTED" ) );
+  }
+
+  @Test(groups="all")
+  public void startsWithCaseSensitiveMatch() {
+    assertTrue( csblacklist.startsWith().test( "blacklisted TEXT" ) );
+  }
+
+  @Test(groups="all")
+  public void startsWithCaseInsensitiveMatch() {
+    assertTrue( csblacklist.startsWith( true ).test( "blackLISTED TEXT" ) );
+  }
+
+  @Test(groups="all")
+  public void endsWithCaseSensitiveMatch() {
+    assertTrue( csblacklist.endsWith().test( "TEXT blacklisted" ) );
+  }
+
+  @Test(groups="all")
+  public void endsWithCaseInsensitiveMatch() {
+    assertTrue( csblacklist.endsWith( true ).test( "TEXT blackLISTED" ) );
+  }
+
+  @Test(groups="all")
+  public void containsCaseSensitiveMatch() {
+    assertTrue( csblacklist.contains().test( "TEXTA blacklisted TEXTB" ) );
+  }
+
+  @Test(groups="all")
+  public void containsCaseInsensitiveMatch() {
+    assertTrue( csblacklist.contains( true ).test( "TEXTA blackLISTEDTEXTB" ) );
+  }
+
+  @Test(groups="all")
+  public void cleanTextCaseSensitive() {
+    String text = IoFunctions.readTextFully( MiscFunctions.getResource( "/blacklist/text_01.txt" ) );
+    assertNotNull( text );
+    assertFalse( text.equals( expected ) );
+    assertFalse( csblacklist.test( text ) );
+    assertThat( csblacklist.cleanup().apply( text ).toString(), is( expected ) );
+  }
+
+  @Test(groups="all")
+  public void cleanTextCaseInsensitive() {
+    String text = IoFunctions.readTextFully( MiscFunctions.getResource( "/blacklist/text_02.txt" ) );
+    assertNotNull( text );
+    assertFalse( text.equals( expected ) );
+    assertTrue( ciblacklist.contains( true ).test( text ) );
+    assertThat( ciblacklist.cleanup( true ).apply( text ).toString(), is( expected ) );
+  }
+
+  @Test(groups="all")
+  public void cleanAndListTextCaseSensitive() {
+    String text = IoFunctions.readTextFully( MiscFunctions.getResource( "/blacklist/text_01.txt" ) );
+    assertNotNull( text );
+    assertFalse( text.equals( expected ) );
+    assertFalse( csblacklist.test( text ) );
+    Set<String> collector = new HashSet<>();
+    assertThat( csblacklist.cleanup( collector::add ).apply( text ).toString(), is( expected ) );
+    assertThat( collector.size(), is(2) );
+    assertTrue( collector.contains( "blacklisted" ) );
+    assertTrue( collector.contains( "blacklisted2" ) );
+  }
+
+  @Test(groups="all")
+  public void cleanAndListTextCaseInsensitive() {
+    String text = IoFunctions.readTextFully( MiscFunctions.getResource( "/blacklist/text_02.txt" ) );
+    assertNotNull( text );
+    assertFalse( text.equals( expected ) );
+    assertTrue( ciblacklist.contains( true ).test( text ) );
+    Set<String> collector = new HashSet<>();
+    assertThat( ciblacklist.cleanup( true, collector::add ).apply( text ).toString(), is( expected ) );
+    assertThat( collector.size(), is(2) );
+    assertTrue( collector.contains( "blacklisted" ) );
+    assertTrue( collector.contains( "blacklisted2" ) );
+  }
+
+  @Test(groups="all")
+  public void cleanAndCountTextCaseSensitive() {
+    String text = IoFunctions.readTextFully( MiscFunctions.getResource( "/blacklist/text_01.txt" ) );
+    assertNotNull( text );
+    assertFalse( text.equals( expected ) );
+    assertFalse( csblacklist.test( text ) );
+    Map<String,Integer> counts = new HashMap<>();
+    assertThat( csblacklist.cleanup( $ -> statistic( counts, $ ) ).apply( text ).toString(), is( expected ) );
+    assertThat( counts.size(), is(2) );
+    assertTrue( counts.containsKey( "blacklisted" ) );
+    assertTrue( counts.containsKey( "blacklisted2" ) );
+    assertThat( counts.get( "blacklisted" ), is( Integer.valueOf(2) ) );
+    assertThat( counts.get( "blacklisted2" ), is( Integer.valueOf(1) ) );
+  }
+
+  @Test(groups="all")
+  public void cleanAndCountTextCaseInsensitive() {
+    String text = IoFunctions.readTextFully( MiscFunctions.getResource( "/blacklist/text_02.txt" ) );
+    assertNotNull( text );
+    assertFalse( text.equals( expected ) );
+    assertTrue( ciblacklist.contains( true ).test( text ) );
+    Map<String,Integer> counts = new HashMap<>();
+    assertThat( ciblacklist.cleanup( true, $ -> statistic( counts, $ ) ).apply( text ).toString(), is( expected ) );
+    assertThat( counts.size(), is(2) );
+    assertTrue( counts.containsKey( "blacklisted" ) );
+    assertTrue( counts.containsKey( "blacklisted2" ) );
+    assertThat( counts.get( "blacklisted" ), is( Integer.valueOf(2) ) );
+    assertThat( counts.get( "blacklisted2" ), is( Integer.valueOf(1) ) );
+  }
+  
+  private void statistic( Map<String,Integer> values, String literal ) {
+    if( values.containsKey( literal ) ) {
+      values.put( literal, Integer.valueOf( values.get( literal ).intValue() + 1 ) );
+    } else {
+      values.put( literal, Integer.valueOf(1) );
+    }
+  }
+
+} /* ENDCLASS */
