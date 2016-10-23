d6ec088be3eb0d2e277b0b016b189bfe7aac32cb
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2016-10-23 21:52:03 +0200

    + a thread factory supporting a customized naming scheme

---
+++ com.kasisoft.libs.common/src/main/java/com/kasisoft/libs/common/thread/CustomThreadFactory.java
@@ -0,0 +1,65 @@
+package com.kasisoft.libs.common.thread;
+
+import lombok.experimental.*;
+
+import lombok.*;
+
+import java.util.concurrent.atomic.*;
+
+import java.util.concurrent.*;
+
+/**
+ * This {@link ThreadFactory} essentially resembles the default implementation with the difference that it
+ * allows to configure a naming pattern.
+ * 
+ * @author daniel.kasmeroglu@kasisoft.net
+ */
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CustomThreadFactory implements ThreadFactory {
+
+  static final AtomicInteger POOLNUMBER = new AtomicInteger(1);
+  
+  ThreadGroup     group;
+  AtomicInteger   threadNumber;
+  String          naming;
+  int             poolNum;
+
+  /**
+   * Initializes this factory.
+   */
+  public CustomThreadFactory() {
+    this( "pool-%1$d-thread-%2$d" );
+  }
+
+  /**
+   * Initializes this factory using the supplied naming format. The format must use indexes to reference the 
+   * arguments. There are currently two arguments:
+   * 
+   * <ul>
+   *   <li>pool number : int</li>
+   *   <li>thread number : int</li>
+   * </ul>
+   * 
+   * @param namingFmt   The formatting String for the thread creation. Not <code>null</code>.
+   */
+  public CustomThreadFactory( @NonNull String namingFmt ) {
+      SecurityManager sm = System.getSecurityManager();
+      threadNumber       = new AtomicInteger(1);
+      group              = (sm != null) ? sm.getThreadGroup() : Thread.currentThread().getThreadGroup();
+      poolNum            = POOLNUMBER.getAndIncrement();
+      naming             = namingFmt;
+  }
+
+  @Override
+  public Thread newThread( Runnable r ) {
+    Thread result = new Thread( group, r, String.format( naming, poolNum, threadNumber.getAndIncrement() ), 0 );
+    if( result.isDaemon() ) {
+      result.setDaemon( false );
+    }
+    if( result.getPriority() != Thread.NORM_PRIORITY ) {
+      result.setPriority( Thread.NORM_PRIORITY );
+    }
+    return result;
+  }
+
+} /* ENDCLASS */
