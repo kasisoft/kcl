42510c6609ab245eacedfb22d1d6d1173db5669d
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2017-03-15 03:29:57 +0100

    ~ changed lombok from '1.14.8' to '1.16.14'

--- com.kasisoft.libs.common/src/main/java/com/kasisoft/libs/common/constants/Empty.java
+++ com.kasisoft.libs.common/src/main/java/com/kasisoft/libs/common/constants/Empty.java
@@ -1,19 +1,14 @@
 package com.kasisoft.libs.common.constants;
 
-import lombok.experimental.*;
-
-import lombok.*;
-
 /**
  * Collection of constants used to indicate empty elements.
  */
-@FieldDefaults(level = AccessLevel.PUBLIC)
 public class Empty {
 
-  static final Object[] NO_OBJECTS = new Object[0];
+  public static final Object[] NO_OBJECTS = new Object[0];
   
-  static final String[] NO_STRINGS = new String[0];
+  public static final String[] NO_STRINGS = new String[0];
   
-  static final String NO_STRING = "";
+  public static final String NO_STRING = "";
   
 } /* ENDCLASS */
--- com.kasisoft.libs.common/src/main/java/com/kasisoft/libs/common/internal/Messages.java
+++ com.kasisoft.libs.common/src/main/java/com/kasisoft/libs/common/internal/Messages.java
@@ -2,59 +2,54 @@ package com.kasisoft.libs.common.internal;
 
 import com.kasisoft.libs.common.i18n.*;
 
-import lombok.experimental.*;
-
-import lombok.*;
-
 /**
  * Collection of messages that could be overridden if necessary.
  * 
  * @author daniel.kasmeroglu@kasisoft.net
  */
-@FieldDefaults(level = AccessLevel.PUBLIC)
 public class Messages {
 
   @I18N("The number of columns for each line isn't consistent !")
-  static String          error_csv_inconsistent_column_count;
+  public static String          error_csv_inconsistent_column_count;
 
   @I18N("Cannot add row %d (data = %s). Error: %s !")
-  static I18NFormatter   error_csv_invalid_add_row;
+  public static I18NFormatter   error_csv_invalid_add_row;
 
   @I18N("The cell value '%s' at column %d cannot be parsed !")
-  static I18NFormatter   error_csv_invalid_cell_value;
+  public static I18NFormatter   error_csv_invalid_cell_value;
 
   @I18N("Misconfigured CsvOptions. The column %d does not provide an adapter !")
-  static I18NFormatter   error_csv_missing_adapter;
+  public static I18NFormatter   error_csv_missing_adapter;
   
   @I18N("The closing quote for content '%s' is missing !")
-  static I18NFormatter   error_csv_missing_closing_quote;
+  public static I18NFormatter   error_csv_missing_closing_quote;
   
   @I18N("The filesystem watching dispatcher has already been started.")
-  static String          error_fswatcher_already_started;
+  public static String          error_fswatcher_already_started;
 
   @I18N("Start the configuration of an argument first (flag, single, many) !")
-  static String          error_missing_argument;
+  public static String          error_missing_argument;
   
   @I18N("Missing required command line option '%s' !")
-  static I18NFormatter   error_missing_required_option;
+  public static I18NFormatter   error_missing_required_option;
 
   @I18N("(default=%s)")
-  static I18NFormatter   format_default;
+  public static I18NFormatter   format_default;
   
   @I18N("%s is not a valid Color")
-  static I18NFormatter   invalid_color;
+  public static I18NFormatter   invalid_color;
   
   @I18N("mandatory")
-  static String          label_mandatory;
+  public static String          label_mandatory;
 
   @I18N("optional")
-  static String          label_optional;
+  public static String          label_optional;
   
   @I18N("Unsupported CharSequence type '%s'")
-  static I18NFormatter   unsupported_charsequence;
+  public static I18NFormatter   unsupported_charsequence;
 
   @I18N("The argument '%s' had not been used !")
-  static I18NFormatter   unused_argument;
+  public static I18NFormatter   unused_argument;
 
   static {
     I18NSupport.initialize( Messages.class );
