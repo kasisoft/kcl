0266f596c08ecf1cf705231d25d4bce1de8cbe86
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2017-03-16 01:10:34 +0100

    ~ directly exposing status messages now

--- com.kasisoft.libs.common/src/main/java/com/kasisoft/libs/common/constants/HttpStatusCode.java
+++ com.kasisoft.libs.common/src/main/java/com/kasisoft/libs/common/constants/HttpStatusCode.java
@@ -56,7 +56,48 @@ public enum HttpStatusCode {
   Unauthorized                  ( 401, "Unauthorized"                     ),
   UnsupportedMediaType          ( 415, "Unsupported Media Type"           ),
   UseProxy                      ( 305, "Use Proxy"                        );
-  
+
+  public static final String SM_ACCEPTED                          = "Accepted";
+  public static final String SM_BAD_GATEWAY                       = "Bad Gateway";
+  public static final String SM_BAD_REQUEST                       = "Bad Request";
+  public static final String SM_CONFLICT                          = "Conflict";
+  public static final String SM_CONTINUE                          = "Continue";
+  public static final String SM_CREATED                           = "Created";
+  public static final String SM_EXPECTATION_FAILED                = "Expectation Failed";
+  public static final String SM_FORBIDDEN                         = "Forbidden";
+  public static final String SM_FOUND                             = "Found";
+  public static final String SM_GATEWAY_TIME_OUT                  = "Gateway Time-out";
+  public static final String SM_GONE                              = "Gone";
+  public static final String SM_HTTP_VERSION_NOT_SUPPORTED        = "HTTP Version not supported";
+  public static final String SM_INTERNAL_SERVER_ERROR             = "Internal Server Error";
+  public static final String SM_LENGHT_REQUIRED                   = "Length Required";
+  public static final String SM_METHOD_NOT_ALLOWED                = "Method Not Allowed";
+  public static final String SM_MOVED_PERMANENTLY                 = "Moved Permanently";
+  public static final String SM_MULTIPLE_CHOICES                  = "Multiple Choices";
+  public static final String SM_NO_CONTENT                        = "No Content";
+  public static final String SM_NON_AUTHORITATIVE_INFORMATION     = "Non-Authoritative Information";
+  public static final String SM_NOT_ACCEPTABLE                    = "Not Acceptable";
+  public static final String SM_NOT_FOUND                         = "Not Found";
+  public static final String SM_NOT_IMPLEMENTED                   = "Not Implemented";
+  public static final String SM_NOT_MODIFIED                      = "Not Modified";
+  public static final String SM_OK                                = "OK";
+  public static final String SM_PARTIAL_CONTENT                   = "Partial Content";
+  public static final String SM_PAYMENT_REQUIRED                  = "Payment Required";
+  public static final String SM_PRECONDITION_FAILED               = "Precondition Failed";
+  public static final String SM_PROXY_AUTHENTICATION_REQUIRED     = "Proxy Authentication Required";
+  public static final String SM_REQUESTED_RANGE_NOT_SATISFIABLE   = "Requested range not satisfiable";
+  public static final String SM_REQUEST_ENTITY_TOO_LARGE          = "Request Entity Too Large";
+  public static final String SM_REQUEST_TIME_OUT                  = "Request Time-out";
+  public static final String SM_REQUEST_URI_TOO_LARGE             = "Request-URI Too Large";
+  public static final String SM_RESET_CONTENT                     = "Reset Content";
+  public static final String SM_SEE_OTHER                         = "See Other";
+  public static final String SM_SERVICE_UNAVAILABLE               = "Service Unavailable";
+  public static final String SM_SWITCHING_PROTOCOLS               = "Switching Protocols";
+  public static final String SM_TEMPORARY_REDIRECT                = "Temporary Redirect";
+  public static final String SM_UNAUTHORIZED                      = "Unauthorized";
+  public static final String SM_UNSUPPORTED_MEDIA_TYPE            = "Unsupported Media Type";
+  public static final String SM_USE_PROXY                         = "Use Proxy";
+
   @Getter int      code;
   
   /** Neither <code>null</code> nor empty. */
