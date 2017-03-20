697e4f2cf35103dca9a847ae6fc5308a1aa9cbf0
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2017-03-20 11:57:05 +0100

    + added an annotation to provide a priority and additional comparator implementations

---
+++ com.kasisoft.libs.common/src/main/java/com/kasisoft/libs/common/comparator/AbstractStringComparator.java
@@ -0,0 +1,38 @@
+package com.kasisoft.libs.common.comparator;
+
+import java.util.function.*;
+
+import java.util.*;
+
+import lombok.experimental.*;
+
+import lombok.*;
+
+/**
+ * @author daniel.kasmeroglu@kasisoft.net
+ */
+/**
+ * Abstract comparator.
+ */
+@FieldDefaults(level = AccessLevel.PRIVATE)
+abstract class AbstractStringComparator<T> implements Comparator<T> {
+
+  BiFunction<String, String, Integer>   comparison;
+  
+  public AbstractStringComparator( boolean ignoreCase ) {
+    comparison = ignoreCase ? this::compareToIgnoreCase : this::compareTo;
+  }
+  
+  protected int compare( String s1, String s2 ) {
+    return comparison.apply( s1, s2 );
+  }
+  
+  private int compareToIgnoreCase( String s1, String s2 ) {
+    return s1.compareToIgnoreCase( s2 );
+  }
+  
+  private int compareTo( String s1, String s2 ) {
+    return s1.compareTo( s2 );
+  }
+
+} /* ENDCLASS */
