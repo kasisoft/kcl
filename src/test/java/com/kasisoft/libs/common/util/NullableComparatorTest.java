103789a699acbae52905d1a0672156b675c3dd98
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2017-09-08 03:03:46 +0200

    ~ fix: the testcase was useless prior to now as the dataset was always empty (refactoring leftover)

--- src/test/java/com/kasisoft/libs/common/util/NullableComparatorTest.java
+++ src/test/java/com/kasisoft/libs/common/util/NullableComparatorTest.java
@@ -23,38 +23,40 @@ public class NullableComparatorTest {
 
   static final int NULLCOUNT = 10;
   
-  private ExtArrayList<Integer>   data;
-  
-  @BeforeMethod
-  public void setup() {
-    data      = new ExtArrayList<>();
-    int nulls = 0;
+  private ExtArrayList<Integer> createInput() {
+    ExtArrayList<Integer> result = new ExtArrayList<>();
+    int                   nulls  = 0;
     while( nulls < NULLCOUNT ) {
       int irand = (int) (Math.random() * 1000);
       if( irand % 3 == 0 ) {
-        data.add( null );
+        result.add( null );
         nulls++;
       } else {
-        data.add( Integer.valueOf( irand ) );
+        result.add( Integer.valueOf( irand ) );
       }
     }
+    return result;
   }
   
   @Test
   public void sortWithoutDelegate() {
+    
+    ExtArrayList<Integer> data = createInput();
     
     Collections.sort( data, new NullableComparator<Integer>() );
     int size = data.size();
     data.trim();
     assertThat( data.size(), is( size - NULLCOUNT ) );
     for( int i = 1; i < data.size(); i++ ) {
-      assertTrue( data.get(i - 1).intValue() >= data.get(i).intValue() );
+      assertTrue( data.get(i - 1).intValue() <= data.get(i).intValue() );
     }
     
   }
 
   @Test
   public void sortWithDelegate() {
+    
+    ExtArrayList<Integer> data = createInput();
     
     Comparator<Integer> delegate = new Comparator<Integer>() {
       @Override
@@ -68,7 +70,7 @@ public class NullableComparatorTest {
     data.trim();
     assertThat( data.size(), is( size - NULLCOUNT ) );
     for( int i = 1; i < data.size(); i++ ) {
-      assertTrue( data.get(i - 1).intValue() <= data.get(i).intValue() );
+      assertTrue( data.get(i - 1).intValue() >= data.get(i).intValue() );
     }
     
   }
