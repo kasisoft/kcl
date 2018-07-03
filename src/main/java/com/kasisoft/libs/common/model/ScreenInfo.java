d763212dd73778a4c3522dafab6f5f35934ab733
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2018-07-03 21:33:05 +0200

    + added comparison functions

--- src/main/java/com/kasisoft/libs/common/model/ScreenInfo.java
+++ src/main/java/com/kasisoft/libs/common/model/ScreenInfo.java
@@ -2,6 +2,8 @@ package com.kasisoft.libs.common.model;
 
 import com.kasisoft.libs.common.util.*;
 
+import java.util.*;
+
 import lombok.*;
 
 /**
@@ -11,19 +13,37 @@ import lombok.*;
 @ToString
 public class ScreenInfo {
 
+  public static final Comparator<ScreenInfo> BY_RATIO       = ScreenInfo::compareByRatio;
+
+  public static final Comparator<ScreenInfo> BY_PIXEL_COUNT = ScreenInfo::compareByPixelCount;
+  
   String   id;
   int      width;
   int      height;
   int      ratioX;
   int      ratioY;
+  int      pixels;
   
   public ScreenInfo( String idString, int sWidth, int sHeight ) {
     id      = idString;
     width   = sWidth;
     height  = sHeight;
+    pixels  = width * height;
     int div = MiscFunctions.gcd( width, height );
     ratioX  = width  / div;
     ratioY  = height / div;
+  }
+
+  private static int compareByRatio( ScreenInfo s1, ScreenInfo s2 ) {
+    int result = Integer.compare( s1.ratioX, s2.ratioX );
+    if( result == 0 ) {
+      result = Integer.compare( s1.ratioY, s2.ratioY );
+    }
+    return result;
+  }
+  
+  private static int compareByPixelCount( ScreenInfo s1, ScreenInfo s2 ) {
+    return Integer.compare( s1.pixels, s2.pixels );
   }
   
 } /* ENDCLASS */
