2a8e95c0db2e3caf470442153e28869e08535f8c
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2018-07-15 17:52:38 +0200

    + added 'adapt' and 'errorHandling' to deal with varieties of 'Consumer'/'Function' implementations

--- src/main/java/com/kasisoft/libs/common/function/Functions.java
+++ src/main/java/com/kasisoft/libs/common/function/Functions.java
@@ -1,8 +1,8 @@
 package com.kasisoft.libs.common.function;
 
-import lombok.*;
-
 import java.util.function.*;
+
+import lombok.*;
 
 /**
  * Collection of useful functions.
@@ -24,6 +24,46 @@ public class Functions {
    */
   public static <T> Function<T, T> nullSafe( @NonNull Function<T, T> delegate ) {
     return $ -> $ != null ? delegate.apply($) : null;
+  }
+  
+  public static <A, B, C, R> TriFunction<A, B, C, R> adapt( Function<A, R> function ) {
+    return ($a, $b, $c) -> function.apply($a);
+  }
+
+  public static <A, B, C, R> TriFunction<A, B, C, R> adapt( BiFunction<A, B, R> function ) {
+    return ($a, $b, $c) -> function.apply($a, $b);
+  }
+
+  public static <A, B, C> TriConsumer<A, B, C> adapt( Consumer<A> consumer ) {
+    return ($a, $b, $c) -> consumer.accept($a);
+  }
+
+  public static <A, B, C> TriConsumer<A, B, C> adapt( BiConsumer<A, B> consumer ) {
+    return ($a, $b, $c) -> consumer.accept($a, $b);
+  }
+
+  public static <E extends Exception> void errorHandling( E exception, Consumer<E> errHandler, Consumer<E> defaultErrHandler ) {
+    if( errHandler != null ) {
+      errHandler.accept( exception );
+    } else if( defaultErrHandler != null ) {
+      defaultErrHandler.accept( exception );
+    }
+  }
+  
+  public static <E extends Exception, C1> void errorHandling( E exception, C1 arg1, BiConsumer<E, C1> errHandler, BiConsumer<E, C1> defaultErrHandler ) {
+    if( errHandler != null ) {
+      errHandler.accept( exception, arg1 );
+    } else if( defaultErrHandler != null ) {
+      defaultErrHandler.accept( exception, arg1 );
+    }
+  }
+
+  public static <E extends Exception, C1, C2> void errorHandling( E exception, C1 arg1, C2 arg2, TriConsumer<E, C1, C2> errHandler, TriConsumer<E, C1, C2> defaultErrHandler ) {
+    if( errHandler != null ) {
+      errHandler.accept( exception, arg1, arg2 );
+    } else if( defaultErrHandler != null ) {
+      defaultErrHandler.accept( exception, arg1, arg2 );
+    }
   }
   
 } /* ENDCLASS */
