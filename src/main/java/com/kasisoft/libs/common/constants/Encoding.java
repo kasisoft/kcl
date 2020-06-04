ca7685a55b3cfcb6f66ecf291468462982d10725
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2020-06-04 22:43:20 +0200

    ~ reestablished 'Encoding'

--- src/main/java/com/kasisoft/libs/common/constants/Encoding.java
+++ src/main/java/com/kasisoft/libs/common/constants/Encoding.java
@@ -1,28 +1,31 @@
 package com.kasisoft.libs.common.constants;
 
+import com.kasisoft.libs.common.KclException;
 import com.kasisoft.libs.common.annotation.Specification;
 import com.kasisoft.libs.common.old.constants.ByteOrderMark;
-import com.kasisoft.libs.common.old.io.IoFunctions;
 
-import java.util.Hashtable;
+import javax.validation.constraints.NotNull;
+
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import java.net.URL;
 
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.PrintStream;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.io.Writer;
@@ -32,16 +35,14 @@ import lombok.experimental.FieldDefaults;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.ToString;
-import lombok.val;
 
 /**
  * Collection of supported encodings.
  * 
  * @author daniel.kasmeroglu@kasisoft.net
  */
-@Specification(value = "http://docs.oracle.com/javase/8/docs/api/java/nio/charset/Charset.html", date = "10-Jun-2016")
+@Specification(value = "https://docs.oracle.com/javase/10/docs/api/java/nio/charset/Charset.html", date = "04-JUN-2020")
 @EqualsAndHashCode(of = "encoding")
 @ToString(of = "encoding")
 @FieldDefaults(level = AccessLevel.PRIVATE)
@@ -54,16 +55,16 @@ public final class Encoding {
   public static final Encoding UTF16LE;
   public static final Encoding ISO88591;
   
-  private static final Map<String,Encoding>   ENCODINGS;
+  private static final Map<String, Encoding>   ENCODINGS;
   
   static {
-    ENCODINGS   = new Hashtable<>();
-    ASCII       = new Encoding( "US-ASCII"    , false , null                  );
-    UTF8        = new Encoding( "UTF-8"       , false , ByteOrderMark.UTF8    );
-    UTF16       = new Encoding( "UTF-16"      , true  , null                  );
-    UTF16BE     = new Encoding( "UTF-16BE"    , false , ByteOrderMark.UTF16BE );
-    UTF16LE     = new Encoding( "UTF-16LE"    , false , ByteOrderMark.UTF16LE );
-    ISO88591    = new Encoding( "ISO-8859-1"  , false , null                  );
+    ENCODINGS   = new HashMap<>();
+    ASCII       = new Encoding("US-ASCII"    , false , null                 );
+    UTF8        = new Encoding("UTF-8"       , false , ByteOrderMark.UTF8   );
+    UTF16       = new Encoding("UTF-16"      , true  , null                 );
+    UTF16BE     = new Encoding("UTF-16BE"    , false , ByteOrderMark.UTF16BE);
+    UTF16LE     = new Encoding("UTF-16LE"    , false , ByteOrderMark.UTF16LE);
+    ISO88591    = new Encoding("ISO-8859-1"  , false , null                 );
   }
   
   /** Neither <code>null</code> nor empty. */
@@ -84,12 +85,14 @@ public final class Encoding {
    * @param requiresbom   <code>true</code> <=> Identifying this encoding requires a {@link ByteOrderMark}. 
    * @param mark          A {@link ByteOrderMark} which allows to identify the encoding. Maybe <code>null</code>.
    */
-  public Encoding( @NonNull String key, boolean requiresbom, ByteOrderMark mark ) {
+  public Encoding(@NotNull String key, boolean requiresbom, ByteOrderMark mark) {
     encoding      = key;
     bomRequired   = requiresbom;
     byteOrderMark = mark;
-    charset       = Charset.forName( key );
-    ENCODINGS.put( key, this );
+    charset       = Charset.forName(key);
+    synchronized (ENCODINGS) {
+      ENCODINGS.put(key, this);
+    }
   }
   
   /**
@@ -98,11 +101,9 @@ public final class Encoding {
    * @param file   The file that has to be opened using this encoding. Must be a valid file.
    *  
    * @return   The reader if the file could be opened. Not <code>null</code>.
-   * 
-   * @throws FailureException if opening the file failed for some reason.
    */
-  public Reader openReader( @NonNull File file ) {
-    return openReader( IoFunctions.newInputStream( file ) );
+  public @NotNull Reader openReader(@NotNull File file) {
+    return openReader(file.toPath());
   }
 
   /**
@@ -111,11 +112,14 @@ public final class Encoding {
    * @param url   The url of the resource that has to be opened using this encoding. Must be a valid resource.
    *  
    * @return   The reader if the url could be opened. Not <code>null</code>.
-   * 
-   * @throws FailureException if opening the resource failed for some reason.
    */
-  public Reader openReader( @NonNull URL url ) {
-    return openReader( IoFunctions.newInputStream( url ) );
+  @SuppressWarnings("resource")
+  public @NotNull Reader openReader(@NotNull URL url) {
+    try {
+      return openReader(url.openStream());
+    } catch (IOException ex) {
+      throw new KclException(ex, "Could not open '%s'!", url);
+    }
   }
 
   /**
@@ -124,11 +128,14 @@ public final class Encoding {
    * @param path   The path of the resource that has to be opened using this encoding. Must be a valid resource.
    *  
    * @return   The reader if the url could be opened. Not <code>null</code>.
-   * 
-   * @throws FailureException if opening the resource failed for some reason.
    */
-  public Reader openReader( @NonNull Path path ) {
-    return openReader( IoFunctions.newInputStream( path ) );
+  @SuppressWarnings("resource")
+  public @NotNull Reader openReader(@NotNull Path path) {
+    try {
+      return openReader(Files.newInputStream(path));
+    } catch (IOException ex) {
+      throw new KclException(ex, "Could not open '%s'!", path);
+    }
   }
 
   /**
@@ -138,10 +145,10 @@ public final class Encoding {
    *  
    * @return   The Reader if it can be accessed. Not <code>null</code>.
    */
-  public Reader openReader( @NonNull InputStream instream ) {
+  public @NotNull Reader openReader(@NotNull InputStream instream) {
     try {
-      return new BufferedReader( new InputStreamReader( instream, encoding ) );
-    } catch( UnsupportedEncodingException ex ) {
+      return new BufferedReader(new InputStreamReader(instream, encoding));
+    } catch (UnsupportedEncodingException ex) {
       // won't happen as we only support guarantueed encodings
       return null;
     }
@@ -153,11 +160,9 @@ public final class Encoding {
    * @param file   The file that has to be opened using this encoding. Must be a valid file.
    *  
    * @return   The writer if the file could be opened. Not <code>null</code>.
-   * 
-   * @throws FailureException if opening the file failed for some reason.
    */
-  public Writer openWriter( @NonNull File file ) {
-    return openWriter( IoFunctions.newOutputStream( file ) );
+  public @NotNull Writer openWriter(@NotNull File file) {
+    return openWriter(file.toPath());
   }
 
   /**
@@ -166,11 +171,14 @@ public final class Encoding {
    * @param path   The path that has to be opened using this encoding. Must be a valid file.
    *  
    * @return   The writer if the file could be opened. Not <code>null</code>.
-   * 
-   * @throws FailureException if opening the file failed for some reason.
    */
-  public Writer openWriter( @NonNull Path path ) {
-    return openWriter( IoFunctions.newOutputStream( path ) );
+  @SuppressWarnings("resource")
+  public @NotNull Writer openWriter(@NotNull Path path) {
+    try {
+      return openWriter(Files.newOutputStream(path));
+    } catch (IOException ex) {
+      throw new KclException(ex, "Failed to open '%s'!", path);
+    }
   }
 
   /**
@@ -180,39 +188,10 @@ public final class Encoding {
    *  
    * @return   The writer if the file could be opened. Not <code>null</code>.
    */
-  public Writer openWriter( @NonNull OutputStream outstream ) {
+  public @NotNull Writer openWriter(@NotNull OutputStream outstream) {
     try {
-      return new BufferedWriter( new OutputStreamWriter( outstream, encoding ) );
-    } catch( UnsupportedEncodingException ex ) {
-      // won't happen as we only support guarantueed encodings
-      return null;
-    }
-  }
-
-  /**
-   * Opens a PrintStream for a specific file using this encoding.
-   * 
-   * @param file   The file that has to be opened using this encoding. Must be a valid file.
-   *  
-   * @return   The PrintStream if the file could be opened. Not <code>null</code>.
-   * 
-   * @throws FailureException if opening the file failed for some reason.
-   */
-  public PrintStream openPrintStream( @NonNull File file ) {
-    return openPrintStream( IoFunctions.newOutputStream( file ) );
-  }
-
-  /**
-   * Opens a PrintStream for a specific OutputStream using this encoding.
-   * 
-   * @param outstream   The OutputStream that has to be opened using this encoding. Must be a valid file.
-   *  
-   * @return   The PrintStream if the OutputStream could be accessed. Not <code>null</code>.
-   */
-  public PrintStream openPrintStream( @NonNull OutputStream outstream ) {
-    try {
-      return new PrintStream( new BufferedOutputStream( outstream ), true, encoding );
-    } catch( UnsupportedEncodingException ex ) {
+      return new BufferedWriter(new OutputStreamWriter(outstream, encoding));
+    } catch (UnsupportedEncodingException ex) {
       // won't happen as we only support guarantueed encodings
       return null;
     }
@@ -225,10 +204,10 @@ public final class Encoding {
    * 
    * @return   The data which has to be encoded. Not <code>null</code>.
    */
-  public byte[] encode( @NonNull String text ) {
-    ByteBuffer buffer = charset.encode( CharBuffer.wrap( text ) );
-    val        result = new byte[ buffer.limit() ];
-    buffer.get( result );
+  public @NotNull byte[] encode(@NotNull String text) {
+    var buffer = charset.encode(CharBuffer.wrap(text));
+    var result = new byte[buffer.limit()];
+    buffer.get(result);
     return result;
   }
 
@@ -239,189 +218,12 @@ public final class Encoding {
    * 
    * @return   The decoded String. Not <code>null</code>.
    */
-  public String decode( @NonNull byte[] data ) {
-    return charset.decode( ByteBuffer.wrap( data ) ).toString();
+  public @NotNull String decode(@NotNull byte[] data) {
+    return charset.decode(ByteBuffer.wrap(data)).toString();
   }
   
-  /**
-   * Opens a Reader for a specific file using this encoding.
-   * 
-   * @param file       The file that has to be opened using this encoding. Must be a valid file.
-   * @param encoding   The encoding that has to be used. If <code>null</code> the default encoding {@link #UTF8} is used.
-   *  
-   * @return   The reader if the file could be opened. Not <code>null</code>.
-   * 
-   * @throws FailureException if opening the file failed for some reason.
-   */
-  public static Reader openReader( @NonNull File file, Encoding encoding ) {
-    if( encoding == null ) {
-      return UTF8.openReader( file );
-    } else {
-      return encoding.openReader( file );
-    }
-  }
-
-  /**
-   * Opens a Reader for a specific file using this encoding.
-   * 
-   * @param path       The path that has to be opened using this encoding. Must be a valid file.
-   * @param encoding   The encoding that has to be used. If <code>null</code> the default encoding {@link #UTF8} is used.
-   *  
-   * @return   The reader if the file could be opened. Not <code>null</code>.
-   * 
-   * @throws FailureException if opening the file failed for some reason.
-   */
-  public static Reader openReader( @NonNull Path path, Encoding encoding ) {
-    if( encoding == null ) {
-      return UTF8.openReader( path );
-    } else {
-      return encoding.openReader( path );
-    }
-  }
-
-  /**
-   * Opens a Reader for a specific InputStream using this encoding.
-   * 
-   * @param instream   The InputStream that has to be opened using this encoding. Not <code>null</code>.
-   * @param encoding   The encoding that has to be used. If <code>null</code> the default encoding {@link #UTF8} is used.
-   *  
-   * @return   The reader if the InputStream could be accessed. Not <code>null</code>.
-   * 
-   * @throws FailureException if accessing the InputStream failed for some reason.
-   */
-  public static Reader openReader( @NonNull InputStream instream, Encoding encoding ) {
-    if( encoding == null ) {
-      return UTF8.openReader( instream );
-    } else {
-      return encoding.openReader( instream );
-    }
-  }
-
-  /**
-   * Opens a Writer for a specific file using this encoding.
-   * 
-   * @param file       The file that has to be opened using this encoding. Must be a valid file.
-   * @param encoding   The encoding that has to be used. If <code>null</code> the default encoding {@link #UTF8} is used.
-   *  
-   * @return   The writer if the file could be opened. Not <code>null</code>.
-   * 
-   * @throws FailureException if opening the file failed for some reason.
-   */
-  public static Writer openWriter( @NonNull File file, Encoding encoding ) {
-    if( encoding == null ) {
-      return UTF8.openWriter( file );
-    } else {
-      return encoding.openWriter( file );
-    }
-  }
-
-  /**
-   * Opens a Writer for a specific file using this encoding.
-   * 
-   * @param path       The path that has to be opened using this encoding. Must be a valid file.
-   * @param encoding   The encoding that has to be used. If <code>null</code> the default encoding {@link #UTF8} is used.
-   *  
-   * @return   The writer if the file could be opened. Not <code>null</code>.
-   * 
-   * @throws FailureException if opening the file failed for some reason.
-   */
-  public static Writer openWriter( @NonNull Path path, Encoding encoding ) {
-    if( encoding == null ) {
-      return UTF8.openWriter( path );
-    } else {
-      return encoding.openWriter( path );
-    }
-  }
-
-  /**
-   * Opens a Writer for a specific OutputStream using this encoding.
-   * 
-   * @param outstream   The OutputStream that has to be opened using this encoding. Not <code>null</code>.
-   * @param encoding    The encoding that has to be used. If <code>null</code> the default encoding {@link #UTF8} is used.
-   *  
-   * @return   The writer if the OutputStream could be accessed. Not <code>null</code>.
-   * 
-   * @throws FailureException if opening the file failed for some reason.
-   */
-  public static Writer openWriter( @NonNull OutputStream outstream, Encoding encoding ) {
-    if( encoding == null ) {
-      return UTF8.openWriter( outstream );
-    } else {
-      return encoding.openWriter( outstream );
-    }
-  }
-
-  /**
-   * Opens a PrintStream for a specific file using this encoding.
-   * 
-   * @param file       The file that has to be opened using this encoding. Must be a valid file.
-   * @param encoding   The encoding that has to be used. If <code>null</code> the default encoding {@link #UTF8} is used.
-   *  
-   * @return   The PrintStream if the file could be opened. Not <code>null</code>.
-   * 
-   * @throws FailureException if opening the file failed for some reason.
-   */
-  public static PrintStream openPrintStream( @NonNull File file, Encoding encoding ) {
-    if( encoding == null ) {
-      return UTF8.openPrintStream( file );
-    } else {
-      return encoding.openPrintStream( file );
-    }
-  }
-
-  /**
-   * Opens a PrintStream for a specific OutputStream using this encoding.
-   * 
-   * @param outstream   The OutputStream that has to be accessed using this encoding. Not <code>null</code>.
-   * @param encoding    The encoding that has to be used. If <code>null</code> the default encoding {@link #UTF8} is 
-   *                    used.
-   *  
-   * @return   The PrintStream if the OutputStream could be accessed. Not <code>null</code>.
-   * 
-   * @throws FailureException if opening the file failed for some reason.
-   */
-  public static PrintStream openPrintStream( @NonNull OutputStream outstream, Encoding encoding ) {
-    if( encoding == null ) {
-      return UTF8.openPrintStream( outstream );
-    } else {
-      return encoding.openPrintStream( outstream );
-    }
-  }
-  
-  /**
-   * Decodes the binary data.
-   * 
-   * @param data       The binary data. Not <code>null</code>.
-   * @param encoding   The encoding which has to be used. If <code>null</code> the default encoding is used.
-   * 
-   * @return   The decoded String. Not <code>null</code>.
-   */
-  public static String decode( @NonNull byte[] data, Encoding encoding ) {
-    if( encoding == null ) {
-      return UTF8.decode( data );
-    } else {
-      return encoding.decode( data );
-    }
-  }
-
-  /**
-   * Encodes the textual data.
-   * 
-   * @param data       The textual data. Not <code>null</code>.
-   * @param encoding   The encoding which has to be used. If <code>null</code> the default encoding is used.
-   * 
-   * @return   The encoded bytes. Not <code>null</code>.
-   */
-  public static byte[] encode( @NonNull String data, Encoding encoding ) {
-    if( encoding == null ) {
-      return UTF8.encode( data );
-    } else {
-      return encoding.encode( data );
-    }
-  }
-
   public static Encoding[] values() {
-    return ENCODINGS.values().toArray( new Encoding[ ENCODINGS.size() ] );
+    return ENCODINGS.values().toArray(new Encoding[ENCODINGS.size()]);
   }
   
   /**
@@ -431,15 +233,10 @@ public final class Encoding {
    * @param name   The name of the encoding which has to be identified. Case sensitivity doesn't matter here.
    *               Neither <code>null</code> nor empty.
    *               
-   * @return   The encoding value or <code>null</code> if it cannot be identified.
+   * @return   The encoding value if available.
    */
-  public static Encoding valueByName( @NonNull String name ) {
-    for( val encoding : Encoding.values() ) {
-      if( encoding.encoding.equalsIgnoreCase( name ) ) {
-        return encoding;
-      }
-    }
-    return null;
+  public static @NotNull Optional<Encoding> findByName(@NotNull String name) {
+    return Optional.ofNullable(ENCODINGS.get(name));
   }
 
 } /* ENDCLASS */
