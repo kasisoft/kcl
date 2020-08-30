5ba6e739d2de8f56be506e91ddbe7f76ffa842ee
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2020-08-30 21:58:58 +0200

    - dropped unnecessary import

--- src/test/java/com/kasisoft/libs/common/AbstractTestCase.java
+++ src/test/java/com/kasisoft/libs/common/AbstractTestCase.java
@@ -6,8 +6,6 @@ import javax.validation.constraints.NotNull;
 
 import java.util.Optional;
 
-import java.net.URL;
-
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
