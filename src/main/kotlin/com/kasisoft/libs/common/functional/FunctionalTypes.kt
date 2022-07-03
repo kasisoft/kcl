de2f323769c7caf61c235993c7dbeb2fc436db6d
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2022-07-03 02:12:23 +0200

    ~ simplified the functional types using the additional helper function 'guard'

--- src/main/kotlin/com/kasisoft/libs/common/functional/FunctionalTypes.kt
+++ src/main/kotlin/com/kasisoft/libs/common/functional/FunctionalTypes.kt
@@ -14,19 +14,20 @@ import com.kasisoft.libs.common.*
 import java.util.function.Function
 import java.util.function.*
 
+private inline fun <T: Any?> guard(op: () -> T): T =
+    try {
+        op()
+    } catch (ex: Exception) {
+        throw KclException.wrap(ex)
+    }
+
+
 fun interface KConsumer<T: Any?> {
 
     @Throws(Exception::class)
     fun accept(t: T)
 
-    fun protect(): Consumer<T> =
-        Consumer<T> { t: T ->
-            try {
-                accept(t)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun protect(): Consumer<T> = Consumer<T> { t: T -> guard { accept(t) } }
 
 } /* ENDINTERFACE */
 
@@ -35,14 +36,7 @@ fun interface KBiConsumer<T: Any?, U: Any?> {
     @Throws(Exception::class)
     fun accept(t: T, u: U)
 
-    fun protect(): BiConsumer<T, U> =
-        BiConsumer<T, U> { t: T, u: U ->
-            try {
-                accept(t, u)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun protect(): BiConsumer<T, U> = BiConsumer<T, U> { t: T, u: U -> guard { accept(t, u) } }
 
 } /* ENDINTERFACE */
 
@@ -57,14 +51,7 @@ fun interface KTriConsumer<T: Any?, U: Any?, Z: Any?> {
     @Throws(Exception::class)
     fun accept(t: T, u: U, z: Z)
 
-    fun protect(): TriConsumer<T, U, Z> =
-        TriConsumer { t: T, u: U, z: Z ->
-            try {
-                accept(t, u, z)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun protect(): TriConsumer<T, U, Z> = TriConsumer { t: T, u: U, z: Z -> guard { accept(t, u, z ) } }
 
 } /* ENDINTERFACE */
 
@@ -73,14 +60,7 @@ fun interface KFunction<T: Any?, R: Any?> {
     @Throws(Exception::class)
     fun apply(t: T): R
 
-    fun protect(): java.util.function.Function<T, R> =
-        Function<T, R> { t: T ->
-            try {
-                apply(t)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun protect(): Function<T, R> = Function<T, R> { t: T -> guard { apply(t) } }
 
 } /* ENDINTERFACE */
 
@@ -89,14 +69,7 @@ fun interface KBiFunction<T: Any?, U: Any?, R: Any?> {
     @Throws(Exception::class)
     fun apply(t: T, u: U): R
 
-    fun protect(): BiFunction<T, U, R> =
-        BiFunction<T, U, R> { t: T, u: U ->
-            try {
-                apply(t, u)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun protect(): BiFunction<T, U, R> = BiFunction<T, U, R> { t: T, u: U -> guard { apply(t, u) } }
 
 } /* ENDINTERFACE */
 
@@ -106,20 +79,12 @@ fun interface TriFunction<T: Any?, U: Any?, Z: Any?, R: Any?> {
 
 } /* ENDINTERFACE */
 
-
 fun interface KTriFunction<T: Any?, U: Any?, Z: Any?, R: Any?> {
 
     @Throws(Exception::class)
     fun apply(t: T, u: U, z: Z): R
 
-    fun protect(): TriFunction<T, U, Z, R> =
-        TriFunction { t: T, u: U, z: Z ->
-            try {
-                apply(t, u, z)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun protect(): TriFunction<T, U, Z, R> =  TriFunction { t: T, u: U, z: Z -> guard { apply(t, u, z) } }
 
 } /* ENDINTERFACE */
 
@@ -128,59 +93,17 @@ fun interface KPredicate<T: Any?> {
     @Throws(Exception::class)
     fun test(t: T): Boolean
 
-    fun protect(): Predicate<T> =
-        Predicate<T> { t: T ->
-            try {
-                test(t)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun protect(): Predicate<T> = Predicate<T> { t: T -> guard { test(t) } }
 
-    fun negate(): Predicate<T> =
-        Predicate { t: T ->
-            try {
-                !test(t)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun negate(): Predicate<T> = Predicate { t: T -> guard { !test(t) } }
 
-    fun and(after: Predicate<T>) =
-        Predicate { t: T ->
-            try {
-                test(t) && after.test(t)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun and(after: Predicate<T>) = Predicate { t: T -> guard { test(t) && after.test(t) } }
 
-    fun and(after: KPredicate<T>) =
-        Predicate { t: T ->
-            try {
-                test(t) && after.test(t)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun and(after: KPredicate<T>) = Predicate { t: T -> guard { test(t) && after.test(t) } }
 
-    fun or(after: Predicate<T>) =
-        Predicate { t: T ->
-            try {
-                test(t) || after.test(t)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun or(after: Predicate<T>) = Predicate { t: T -> guard { test(t) || after.test(t) } }
 
-    fun or(after: KPredicate<T>) =
-        Predicate { t: T ->
-            try {
-                test(t) || after.test(t)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun or(after: KPredicate<T>) = Predicate { t: T -> guard { test(t) || after.test(t) } }
 
 } /* ENDINTERFACE */
 
@@ -189,59 +112,17 @@ fun interface KBiPredicate<T: Any?, U: Any?> {
     @Throws(Exception::class)
     fun test(t: T, u: U): Boolean
 
-    fun protect(): BiPredicate<T, U> =
-        BiPredicate<T, U> { t: T, u: U ->
-            try {
-                test(t, u)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun protect(): BiPredicate<T, U> = BiPredicate<T, U> { t: T, u: U -> guard { test(t, u) } }
 
-    fun negate(): BiPredicate<T, U> =
-        BiPredicate { t: T, u: U ->
-            try {
-                !test(t, u)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun negate(): BiPredicate<T, U> = BiPredicate { t: T, u: U -> guard { !test(t, u) } }
 
-    fun and(after: BiPredicate<T, U>) =
-        BiPredicate { t: T, u: U ->
-            try {
-                test(t, u) && after.test(t, u)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun and(after: BiPredicate<T, U>) = BiPredicate { t: T, u: U -> guard { test(t, u) && after.test(t, u) } }
 
-    fun and(after: KBiPredicate<T, U>) =
-        BiPredicate { t: T, u: U ->
-            try {
-                test(t, u) && after.test(t, u)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun and(after: KBiPredicate<T, U>) = BiPredicate { t: T, u: U -> guard { test(t, u) && after.test(t, u) } }
 
-    fun or(after: BiPredicate<T, U>) =
-        BiPredicate { t: T, u: U ->
-            try {
-                test(t, u) || after.test(t, u)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun or(after: BiPredicate<T, U>) = BiPredicate { t: T, u: U -> guard { test(t, u) || after.test(t, u) } }
 
-    fun or(after: KBiPredicate<T, U>) =
-        BiPredicate { t: T, u: U ->
-            try {
-                test(t, u) || after.test(t, u)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun or(after: KBiPredicate<T, U>) = BiPredicate { t: T, u: U -> guard { test(t, u) || after.test(t, u) } }
 
 } /* ENDINTERFACE */
 
@@ -256,59 +137,17 @@ fun interface KTriPredicate<T: Any?, U: Any?, Z: Any?> {
     @Throws(Exception::class)
     fun test(t: T, u: U, z: Z): Boolean
 
-    fun protect(): TriPredicate<T, U, Z> =
-        TriPredicate { t: T, u: U, z: Z ->
-            try {
-                test(t, u, z)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun protect(): TriPredicate<T, U, Z> = TriPredicate { t: T, u: U, z: Z -> guard { test(t, u, z) } }
 
-    fun negate(): TriPredicate<T, U, Z> =
-        TriPredicate { t: T, u: U, z: Z ->
-            try {
-                !test(t, u, z)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun negate(): TriPredicate<T, U, Z> = TriPredicate { t: T, u: U, z: Z -> guard { !test(t, u, z) } }
 
-    fun and(after: KTriPredicate<T, U, Z>) =
-        TriPredicate { t: T, u: U, z: Z ->
-            try {
-                test(t, u,  z) && after.test(t, u, z)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun and(after: KTriPredicate<T, U, Z>) = TriPredicate { t: T, u: U, z: Z -> guard { test(t, u,  z) && after.test(t, u, z) } }
 
-    fun and(after: TriPredicate<T, U, Z>) =
-        TriPredicate { t: T, u: U, z: Z ->
-            try {
-                test(t, u,  z) && after.test(t, u, z)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun and(after: TriPredicate<T, U, Z>) = TriPredicate { t: T, u: U, z: Z -> guard { test(t, u,  z) && after.test(t, u, z) } }
 
-    fun or(after: KTriPredicate<T, U, Z>) =
-        TriPredicate { t: T, u: U, z: Z ->
-            try {
-                test(t, u,  z) || after.test(t, u, z)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun or(after: KTriPredicate<T, U, Z>) = TriPredicate { t: T, u: U, z: Z -> guard { test(t, u,  z) || after.test(t, u, z) } }
 
-    fun or(after: TriPredicate<T, U, Z>) =
-        TriPredicate { t: T, u: U, z: Z ->
-            try {
-                test(t, u,  z) || after.test(t, u, z)
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun or(after: TriPredicate<T, U, Z>) = TriPredicate { t: T, u: U, z: Z -> guard { test(t, u,  z) || after.test(t, u, z) } }
 
 } /* ENDINTERFACE */
 
@@ -317,14 +156,7 @@ fun interface KSupplier<R: Any?> {
     @Throws(Exception::class)
     fun get(): R
 
-    fun protect(): Supplier<R> =
-        Supplier<R> {
-            try {
-                get()
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun protect(): Supplier<R> = Supplier<R> { guard { get() } }
 
 } /* ENDINTERFACE */
 
@@ -333,14 +165,7 @@ fun interface KRunnable {
     @Throws(Exception::class)
     fun run()
 
-    fun protect(): Runnable =
-        Runnable {
-            try {
-                run()
-            } catch (ex: Exception) {
-                throw KclException.wrap(ex)
-            }
-        }
+    fun protect(): Runnable = Runnable { guard { run() } }
 
 } /* ENDINTERFACE */
 
