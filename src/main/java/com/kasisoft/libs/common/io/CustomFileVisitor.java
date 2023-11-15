b67e30e116ad8ebda52536f23096fac982b5f990
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2023-11-15 20:09:34 +0100

    ~ added the possibility to stop the visiting process

--- src/main/java/com/kasisoft/libs/common/io/CustomFileVisitor.java
+++ src/main/java/com/kasisoft/libs/common/io/CustomFileVisitor.java
@@ -2,64 +2,53 @@ package com.kasisoft.libs.common.io;
 
 import java.util.function.*;
 
-import java.nio.file.*;
 import java.nio.file.attribute.*;
+
+import java.nio.file.*;
 
 import java.io.*;
 
 /**
+ * A basic implementation of a FileVisitor which allows to use consumers as hooks and can be stopped.
+ *
  * @author daniel.kasmeroglu@kasisoft.com
  */
 public class CustomFileVisitor extends SimpleFileVisitor<Path> {
 
     private Function<Exception, FileVisitResult> errorHandler;
-
     private Consumer<Path>                       onPostDirectory;
-
     private Consumer<Path>                       onPreDirectory;
-
     private Consumer<Path>                       onFile;
-
     private Consumer<Path>                       onFileFailed;
+    private boolean                              stop;
 
     public CustomFileVisitor() {
         this.errorHandler    = $ -> FileVisitResult.TERMINATE;
-        this.onPostDirectory = $ -> {
-                             };
-        this.onPreDirectory  = $ -> {
-                             };
-        this.onFile          = $ -> {
-                             };
-        this.onFileFailed    = $ -> {
-                             };
+        this.onPostDirectory = $ -> {};
+        this.onPreDirectory  = $ -> {};
+        this.onFile          = $ -> {};
+        this.onFileFailed    = $ -> {};
+        this.stop            = false;
     }
 
-    public Consumer<Path> getOnPostDirectory() {
-        return onPostDirectory;
+    public void reset() {
+      stop = false;
+    }
+
+    public void stop() {
+      stop = true;
     }
 
     public void setOnPostDirectory(Consumer<Path> onPostDirectory) {
         this.onPostDirectory = onPostDirectory;
     }
 
-    public Consumer<Path> getOnPreDirectory() {
-        return onPreDirectory;
-    }
-
     public void setOnPreDirectory(Consumer<Path> onPreDirectory) {
         this.onPreDirectory = onPreDirectory;
     }
 
-    public Consumer<Path> getOnFile() {
-        return onFile;
-    }
-
     public void setOnFile(Consumer<Path> onFile) {
         this.onFile = onFile;
-    }
-
-    public Consumer<Path> getOnFileFailed() {
-        return onFileFailed;
     }
 
     public void setOnFileFailed(Consumer<Path> onFileFailed) {
@@ -70,15 +59,15 @@ public class CustomFileVisitor extends SimpleFileVisitor<Path> {
         this.errorHandler = errorHandler;
     }
 
-    public Function<Exception, FileVisitResult> getErrorHandler() {
-        return errorHandler;
+    private FileVisitResult fileVisitResult() {
+        return stop ? FileVisitResult.TERMINATE : FileVisitResult.CONTINUE;
     }
 
     @Override
     public FileVisitResult postVisitDirectory(Path dir, IOException io) throws IOException {
         try {
             onPostDirectory.accept(dir);
-            return FileVisitResult.CONTINUE;
+            return fileVisitResult();
         } catch (Exception ex) {
             return errorHandler.apply(ex);
         }
@@ -88,7 +77,7 @@ public class CustomFileVisitor extends SimpleFileVisitor<Path> {
     public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
         try {
             onPreDirectory.accept(dir);
-            return FileVisitResult.CONTINUE;
+            return fileVisitResult();
         } catch (Exception ex) {
             return errorHandler.apply(ex);
         }
@@ -98,7 +87,7 @@ public class CustomFileVisitor extends SimpleFileVisitor<Path> {
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         try {
             onFile.accept(file);
-            return FileVisitResult.CONTINUE;
+            return fileVisitResult();
         } catch (Exception ex) {
             return errorHandler.apply(ex);
         }
@@ -108,7 +97,7 @@ public class CustomFileVisitor extends SimpleFileVisitor<Path> {
     public FileVisitResult visitFileFailed(Path file, IOException ioex) throws IOException {
         try {
             onFileFailed.accept(file);
-            return FileVisitResult.CONTINUE;
+            return fileVisitResult();
         } catch (Exception ex) {
             return errorHandler.apply(ex);
         }
